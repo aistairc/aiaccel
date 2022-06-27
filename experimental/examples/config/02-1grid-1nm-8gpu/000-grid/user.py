c765325f1ccfdefa4f621e5b9c4ebc3295633dd9
koba-22 <105629977+koba-22@users.noreply.github.com>
2022-06-27 19:44:15 +0900

    Directory name change etc.

---
+++ experimental/examples/config/02-1grid-1nm-8gpu/000-grid/user.py
@@ -0,0 +1,32 @@
+import subprocess
+from aiaccel.util import aiaccel
+
+
+def execute(commands):
+    output = subprocess.run(
+        commands,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    if output.stderr is not None:
+        stderr = output.stderr.decode('UTF-8')
+        # エラー処理はしていない。試作中のため。
+    if output.stdout is not None:
+        stdout = output.stdout.decode('UTF-8')
+        return float(stdout.split('\n')[-1])
+    print('Error: user.py: execute()')
+    return None
+
+
+def func(p):
+    x1 = p["x1"]
+    x2 = p["x2"]
+    cmd = './mnist_convnet.sh %f %f' % (x1, x2)
+    y = execute(cmd.split(' '))
+    return y
+
+
+if __name__ == "__main__":
+
+    run = aiaccel.Run()
+    run.execute_and_report(func)
