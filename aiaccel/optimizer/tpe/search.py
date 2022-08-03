d26a395e759a69e8ba2b0422ab1521a3610d43f2
aramoto99 <aramoto-masahiro@aist.go.jp>
2022-08-03 10:16:25 +0900

    Add dynamic loading of optimizer by importlib

--- aiaccel/optimizer/tpe/search.py
+++ aiaccel/optimizer/tpe/search.py
@@ -244,7 +244,7 @@ class Optimizer(AbstractOptimizer):
                 # debug
                 print('Running trial does not match any running files.')
                 print('\ttrial params: ', t.params)
-                raise()
+                raise ()
 
 
 def create_distributions(
--- tests/unit/optimizer_test/test_optimizer.py
+++ tests/unit/optimizer_test/test_optimizer.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import aiaccel
 from aiaccel.optimizer.optimizer import OptimizerLoeader
+from importlib.machinery import SourceFileLoader
 
 
 def test_OptimizerLoeader_1():
@@ -19,7 +20,17 @@ def test_OptimizerLoeader_1():
     algorithm = "aiaccel.optimizer.nelder_mead"
     opt = OptimizerLoeader(algorithm)
     assert opt.get().Optimizer == aiaccel.optimizer.nelder_mead.search.Optimizer
- 
+
     algorithm = "aiaccel.optimizer.tpe"
     opt = OptimizerLoeader(algorithm)
     assert opt.get().Optimizer == aiaccel.optimizer.tpe.search.Optimizer
+
+
+def test_OptimizerLoeader_2():
+    algorithm = Path("./tests/unit/optimizer_test/sample")
+    try:
+        opt = OptimizerLoeader(algorithm)
+        assert True
+
+    except:
+        assert False
