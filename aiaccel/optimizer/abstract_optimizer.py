e39726551bc4b11733dddea9f8f3c8f178e88909
aramoto99 <aramoto-masahiro@aist.go.jp>
2022-10-27 15:08:46 +0900

    Fix typo

--- aiaccel/optimizer/abstract_optimizer.py
+++ aiaccel/optimizer/abstract_optimizer.py
@@ -245,5 +245,5 @@ class AbstractOptimizer(AbstractModule):
         for trial_id in error_trial_ids:
             error_message = self.storage.error.get_any_trial_error(trial_id=trial_id)
             self.logger.error(error_message)
-        
+
         return False
