047ab826bc19e722b1cdfbfdb710ee5771f6d569
aramoto99 <107896581+aramoto99@users.noreply.github.com>
2022-12-01 14:59:43 +0900

    Add test for pylocal mode. (#137)
    * Improved local mode execution speed
    
    * Improved local mode execution speed
    
    * Fix for test
    
    * Fix Comment
    
    * Fix Typo
    
    * refactoring
    
    * test
    
    * fix util/aiaccel.py
    
    * Add new local mode
    
    * Add python local mode test
    
    * Fix fot test
    
    * Fix for test
    
    * Added a new entry to the examples config.yaml.
    
    * The two types of local modes are divided by the unit of source code.
    
    * Added a test to switch between local and pylocal mode.

--- tests/base_test.py
+++ tests/base_test.py
@@ -241,6 +241,8 @@ class BaseTest(object):
             name = f"{d['trial_id']}.yml"
             path = work_dir / 'result' / name
             create_yaml(path, d)
+
+        self.result_comparison = []
         
     @contextmanager
     def create_main(self):
