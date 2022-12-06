be8774941a64a37e9bd988ec32bb20c0b82bc6dc
KanaiYuma <kanai-itc1013@aist.go.jp>
2022-12-06 16:56:36 +0900

    delete original_main variable

--- tests/base_test.py
+++ tests/base_test.py
@@ -189,22 +189,6 @@ d2 = {
     "start_time": "11/03/2020 16:07:40"
 }
 
-original_main = [
-    "import numpy as np",
-    "from aiaccel.util import aiaccel",
-    "def main(p):",
-    "    x = np.array([",
-    "        p['x1'], p['x2'], p['x3'], p['x4'], p['x5'],",
-    "        p['x6'], p['x7'], p['x8'], p['x9'], p['x10']",
-    "    ])",
-    "    # Sphere",
-    "    y = np.sum(x ** 2)",
-    "    return float(y)",
-    "if __name__ == '__main__':",
-    "    run = aiaccel.Run()",
-    "    run.execute_and_report(main)"
-]
-
 
 class BaseTest(object):
 
