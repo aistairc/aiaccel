1f0eac1d980ef5d24c05e2db3bcd4e043ec26065
KanaiYuma <kanai-itc1013@aist.go.jp>
2023-03-02 14:22:55 +0900

    stop optimizer if there is a write to the returncode table

--- aiaccel/optimizer/abstract_optimizer.py
+++ aiaccel/optimizer/abstract_optimizer.py
@@ -288,7 +288,7 @@ class AbstractOptimizer(AbstractModule):
         Returns:
             bool: True if there is no error.
         """
-        error_trial_ids = self.storage.returncode.get_return_code_trial_id(1)
+        error_trial_ids = self.storage.returncode.get_return_code_trial_id()
         if len(error_trial_ids) == 0:
             return True
 
