842b1351216fe6d4add484aa59c7fcff64a2126f
aramoto99 <aramoto-masahiro@aist.go.jp>
2023-03-13 10:16:19 +0900

    fix test

--- tests/supplements/additional_grid_test.py
+++ tests/supplements/additional_grid_test.py
@@ -4,9 +4,8 @@ import subprocess
 from pathlib import Path
 
 import aiaccel
-from aiaccel.config import Config
+from aiaccel.config import load_config
 from aiaccel.storage.storage import Storage
-
 from tests.base_test import BaseTest
 
 
@@ -17,20 +16,20 @@ class AdditionalGridTest(BaseTest):
         test_data_dir = Path(__file__).resolve().parent.joinpath('additional_grid_test', 'test_data')
         config_file = test_data_dir.joinpath('config_{}.yaml'.format(self.search_algorithm))
         config_file = create_tmp_config(config_file)
-        self.config = Config(config_file)
+        self.config = load_config(config_file)
         python_file = test_data_dir.joinpath('user.py')
 
         with self.create_main(python_file):
-            storage = Storage(ws=Path(self.config.workspace.get()))
+            storage = Storage(ws=Path(self.config.generic.workspace))
             subprocess.Popen(['aiaccel-start', '--config', str(config_file), '--clean']
-                             ).wait(timeout=self.config.batch_job_timeout.get())
+                             ).wait(timeout=self.config.generic.batch_job_timeout)
         self.evaluate(work_dir, storage)
 
     def evaluate(self, work_dir, storage):
         running = storage.get_num_running()
         ready = storage.get_num_ready()
         finished = storage.get_num_finished()
-        assert finished == self.config.trial_number.get()
+        assert finished == self.config.optimize.trial_number
         assert ready == 0
         assert running == 0
         final_result = work_dir.joinpath(aiaccel.dict_result, aiaccel.file_final_result)
