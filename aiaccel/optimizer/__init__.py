46ddbd465ff00648304e09bbd5cf76e27c4fe99c
Takashi OKADA <tk.okada.github@gmail.com>
2023-03-20 15:12:29 +0900

    Fix merging.

--- aiaccel/optimizer/__init__.py
+++ aiaccel/optimizer/__init__.py
@@ -1,16 +1,11 @@
-from aiaccel.optimizer.abstract_optimizer import AbstractOptimizer
-from aiaccel.optimizer.grid_optimizer import generate_grid_points
-from aiaccel.optimizer.grid_optimizer import get_grid_options
-from aiaccel.optimizer.grid_optimizer import GridOptimizer
 from aiaccel.optimizer._nelder_mead import NelderMead
+from aiaccel.optimizer.abstract_optimizer import AbstractOptimizer
+from aiaccel.optimizer.create import create_optimizer
+from aiaccel.optimizer.grid_optimizer import GridOptimizer, generate_grid_points, get_grid_options
 from aiaccel.optimizer.nelder_mead_optimizer import NelderMeadOptimizer
 from aiaccel.optimizer.random_optimizer import RandomOptimizer
 from aiaccel.optimizer.sobol_optimizer import SobolOptimizer
-from aiaccel.optimizer.tpe_optimizer import TpeOptimizer
-from aiaccel.optimizer.tpe_optimizer import TPESamplerWrapper
-from aiaccel.optimizer.motpe_optimizer import MOTpeOptimizer
-from aiaccel.optimizer.tpe_optimizer import create_distributions
-from aiaccel.optimizer.create import create_optimizer
+from aiaccel.optimizer.tpe_optimizer import TpeOptimizer, create_distributions
 
 __all__ = [
     'AbstractOptimizer',
