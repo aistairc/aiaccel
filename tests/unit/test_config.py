1fc15103eee82c72e8b4cd5e24e803f86d0ac975
KanaiYuma <kanai-itc1013@aist.go.jp>
2023-03-23 11:29:06 +0900

    add config test

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -10,6 +10,9 @@ class TestConfig(BaseTest):
         config_path = self.test_data_dir.joinpath('config.json')
         config = load_config(str(config_path))
         assert type(config) is omegaconf.dictconfig.DictConfig
+        assert type(config.generic.workspace) is str
+        assert type(config.resource.num_node) is int
+        assert type(config.optimize.parameters[0].lower) is float
 
         # typo config (typo trial_number)
         config_typo_path = self.test_data_dir.joinpath('config_typo.json')
@@ -22,9 +25,21 @@ class TestConfig(BaseTest):
         # customize config (user option)
         config_user_option_path = self.test_data_dir.joinpath('config_user_option.json')
         config = load_config(str(config_user_option_path))
+        # completely new item
         assert type(config) is omegaconf.dictconfig.DictConfig
         assert type(config.user_option.option_str) is str
         assert type(config.user_option.option_int) is int
         assert type(config.user_option.option_bool) is bool
         assert type(config.user_option.option_float) is float
         assert type(config.user_option.option_list) is omegaconf.listconfig.ListConfig
+        # additions to existing entries
+        assert type(config.generic.user_option_generic) is str
+        assert type(config.resource.user_option_resource) is str
+        assert type(config.ABCI.user_option_ABCI) is str
+        assert type(config.optimize.user_option_optimize) is str
+        assert type(config.job_setting.user_option_job_setting) is str
+        assert type(config.logger.file.user_option_file) is str
+        assert type(config.logger.log_level.user_option_log_level) is str
+        assert type(config.logger.stream_level.user_option_stream_level) is str
+        assert type(config.logger.user_option_logger) is str
+        assert type(config.verification.user_option_verification) is str
