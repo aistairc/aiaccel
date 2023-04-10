29ea5790396ef1d0db164955578edc43b10e18b2
Masahiro Aramoto <107896581+aramoto99@users.noreply.github.com>
2023-04-10 12:26:27 +0900

    Add visualization module using TensorBoard. (#267)
    * Added visualization function using TensorBoard.
    
    * Added visualization function using TensorBoard.
    
    * (ci-test-check)
    
    * fix __init__.py
    
    * fix setup.py
    
    * fix-typo
    
    * fix typo

--- aiaccel/__init__.py
+++ aiaccel/__init__.py
@@ -1,6 +1,6 @@
 from aiaccel import (abci, cli, common, config, master, module, optimizer,
-                     parameter, scheduler, storage, util, workspace,
-                     wrapper_tools)
+                     parameter, scheduler, storage, tensorboard, util,
+                     workspace, wrapper_tools)
 
 __all__ = [
     'common',
@@ -15,5 +15,6 @@ __all__ = [
     'optimizer',
     'scheduler',
     'storage',
+    'tensorboard',
     'util'
 ]
