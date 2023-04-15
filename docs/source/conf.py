57c3441f832d3586809e1decd4d0249f13c33e14
Takashi OKADA <tk.okada.github@gmail.com>
2023-04-15 09:41:43 +0900

    update docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,16 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = "aiaccel"
-copyright = "2022, AIST"
-author = "AIST"
+project = 'aiaccel'
+copyright = '2022, AIST'
+author = 'AIST'
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,24 +30,24 @@ release = "0.0.1"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.todo",
-    "sphinx_fontawesome",
-    "sphinx_rtd_theme",
-    "myst_parser",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx_fontawesome',
+    'sphinx_rtd_theme',
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
 ]
 
 # Add optional syntaxes for MyST-Parser which are disabled by default.
@@ -84,7 +83,7 @@ myst_heading_anchors = 3
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_show_sourcelink = False
 html_show_sphinx = False
 
@@ -94,8 +93,8 @@ html_show_sphinx = False
 # html_static_path = ['_static']
 
 gettext_compact = False
-locale_dirs = ["locale/"]
-language = "ja"
+locale_dirs = ['locale/']
+language = 'ja'
 
 # -- Extension configuration -------------------------------------------------
 todo_include_todos = True
