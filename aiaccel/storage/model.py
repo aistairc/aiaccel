9ef1a6768c36d2e7c372ab2fcc42de4ab52c2a57
KanaiYuma <kanai-itc1013@aist.go.jp>
2023-04-19 16:45:49 +0900

    Fix for code formats.

--- aiaccel/storage/model.py
+++ aiaccel/storage/model.py
@@ -56,4 +56,3 @@ class VariableTable(Base):
     process_name = Column(String(length=128), nullable=False)
     label = Column(String(length=128), nullable=False)
     value = Column(PickleType, nullable=False)
-
