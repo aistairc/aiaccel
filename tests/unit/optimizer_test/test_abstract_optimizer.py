8a84d3ec036bfe2a6ea19bcacbe95296539524b1
KanaiYuma <kanai-itc1013@aist.go.jp>
2023-04-19 16:46:42 +0900

    Change tests for is_ignore_warning

--- tests/unit/optimizer_test/test_abstract_optimizer.py
+++ tests/unit/optimizer_test/test_abstract_optimizer.py
@@ -169,7 +169,15 @@ class TestAbstractOptimizer(BaseTest):
         self.optimizer.storage.error.all_delete()
         assert self.optimizer.check_error() is True
 
-        self.optimizer.storage.returncode.set_any_trial_return_code(trial_id=0, return_code=1)
+        self.optimizer.storage.error.set_any_trial_error(trial_id=0, error_message="test warning")
+        assert self.optimizer.check_error() is True
+
+        self.optimizer.storage.error.set_any_trial_exitcode(trial_id=0, exitcode=1)
+        assert self.optimizer.check_error() is False
+
+        self.optimizer.storage.error.all_delete()
+        self.optimizer.config.generic.is_ignore_warning = False
+        self.optimizer.storage.error.set_any_trial_error(trial_id=0, error_message="test warning")
         assert self.optimizer.check_error() is False
 
     def test__serialize(self):
