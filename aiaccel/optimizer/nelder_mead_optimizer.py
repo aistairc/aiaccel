bb0c4132663c6dc20a36c5ed10d584dbe8f1c6d0
aramoto99 <aramoto-masahiro@aist.go.jp>
2023-06-06 09:55:47 +0900

    fix flake8 error

--- aiaccel/optimizer/nelder_mead_optimizer.py
+++ aiaccel/optimizer/nelder_mead_optimizer.py
@@ -6,9 +6,8 @@ from typing import Any
 from omegaconf.dictconfig import DictConfig
 
 from aiaccel.config import is_multi_objective
-from aiaccel.converted_parameter import ConvertedFloatParameter, ConvertedIntParameter, ConvertedParameterConfiguration
+from aiaccel.converted_parameter import ConvertedParameterConfiguration
 from aiaccel.optimizer import AbstractOptimizer, NelderMead
-from aiaccel.parameter import FloatParameter, IntParameter, OrdinalParameter
 
 
 class NelderMeadOptimizer(AbstractOptimizer):
