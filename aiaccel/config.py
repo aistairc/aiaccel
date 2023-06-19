ffb81ca72157110b938ec0aae17085be90205de1
Sota YAMAMOTO <117894869+styamamoto@users.noreply.github.com>
2023-06-19 16:52:09 +0900

    fix type annotation in ParameterConfig (#298)

--- aiaccel/config.py
+++ aiaccel/config.py
@@ -72,8 +72,8 @@ class ParameterConfig:
     upper: Union[float, int]
     # initial: Optional[Union[None, float, int, str, List[Union[float, int]]]]  # Unions of containers are not supported
     initial: Optional[Any]
-    choices: Optional[List[str]]
-    sequence: Optional[List[float]]
+    choices: Optional[List[Union[float, int, str]]]
+    sequence: Optional[List[Union[float, int]]]
     log: Optional[bool]
     base: Optional[int]
     step: Optional[Union[int, float]]
