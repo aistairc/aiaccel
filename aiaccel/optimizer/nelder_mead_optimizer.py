76474413d5e05b6bceeba0b91653dc4b0e8d08bf
aramoto99 <aramoto.masahiro@gmail.com>
2023-10-12 15:54:12 +0900

    Refacotring Nelder-Mead

--- aiaccel/optimizer/nelder_mead_optimizer.py
+++ aiaccel/optimizer/nelder_mead_optimizer.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 import copy
 from typing import Any
 
+import numpy as np
 from omegaconf.dictconfig import DictConfig
+from omegaconf.listconfig import ListConfig
 
+from aiaccel.common import goal_maximize
 from aiaccel.config import is_multi_objective
 from aiaccel.converted_parameter import ConvertedParameterConfiguration
-from aiaccel.optimizer import AbstractOptimizer, NelderMead
+from aiaccel.optimizer import AbstractOptimizer
+from aiaccel.optimizer._nelder_mead import NelderMead, Vertex
+from aiaccel.optimizer.value import Value
+from aiaccel.parameter import OrdinalParameter
 
 
 class NelderMeadOptimizer(AbstractOptimizer):
@@ -21,8 +27,6 @@ class NelderMeadOptimizer(AbstractOptimizer):
     Attributes:
         nelder_mead (NelderMead): A class object implementing Nelder-Mead
             method.
-        parameter_pool (list): A pool of parameters waiting for the process.
-        order (list): A list of parameters being processed.
     """
 
     def __init__(self, config: DictConfig) -> None:
@@ -30,12 +34,52 @@ class NelderMeadOptimizer(AbstractOptimizer):
         self.params: ConvertedParameterConfiguration = ConvertedParameterConfiguration(
             self.params, convert_log=True, convert_int=True, convert_choices=True, convert_sequence=True
         )
+        self.base_params = self.params.get_empty_parameter_dict()
+        self.n_params = len(self.params.get_parameter_list())
+        self.param_names = self.params.get_parameter_names()
+        self.bdrys = np.array([[p.lower, p.upper] for p in self.params.get_parameter_list()])
+        self.n_dim = len(self.bdrys)
         self.nelder_mead: Any = None
-        self.parameter_pool: list[dict[str, Any]] = []
-        self.order: list[int] = []
-
         if is_multi_objective(self.config):
             raise NotImplementedError("Nelder-Mead optimizer does not support multi-objective optimization.")
+        self.single_or_multiple_trial_params: list[Vertex] = []
+        self.map_trial_id_and_vertex_id: dict[int, str] = {}
+        self.completed_trial_ids: list[int] = []
+
+    def convert_ndarray_to_parameter(self, ndarray: np.ndarray[Any, Any]) -> list[dict[str, float | int | str]]:
+        """Convert a list of numpy.ndarray to a list of parameters."""
+        new_params = copy.deepcopy(self.base_params)
+        for name, value, b in zip(self.param_names, ndarray, self.bdrys):
+            for new_param in new_params:
+                if new_param["parameter_name"] == name:
+                    new_param["value"] = value
+                if b[0] <= value <= b[1]:
+                    new_param["out_of_boundary"] = False
+                else:
+                    new_param["out_of_boundary"] = True
+        return new_params
+
+    def new_finished(self) -> list[int]:
+        finished = self.storage.get_finished()
+        return list(set(finished) ^ set(self.completed_trial_ids))
+
+    def _generate_initial_parameter(self, initial_parameters: Any, dim: int, num_of_initials: int) -> Any:
+        params = self.params.get_parameter_list()
+        if initial_parameters is None:
+            if isinstance(params[dim], OrdinalParameter):
+                return self._rng.randint(len(params[dim].sequence))
+            return params[dim].sample(rng=self._rng)["value"]
+
+        if not isinstance(initial_parameters[dim]["value"], (list, ListConfig)):
+            initial_parameters[dim]["value"] = [initial_parameters[dim]["value"]]
+
+        if num_of_initials < len(initial_parameters[dim]["value"]):
+            val = initial_parameters[dim]["value"][num_of_initials]
+            return val
+
+        else:
+            val = params[dim].sample(rng=self._rng)["value"]
+            return val
 
     def generate_initial_parameter(self) -> list[dict[str, float | int | str]] | None:
         """Generate initial parameters.
@@ -44,159 +88,22 @@ class NelderMeadOptimizer(AbstractOptimizer):
             list[dict[str, float | int | str]] | None: A list of new
             parameters. None if `self.nelder_mead` is already defined.
         """
-        initial_parameter = super().generate_initial_parameter()
-        if self.nelder_mead is not None:
-            return None
-
-        self.nelder_mead = NelderMead(
-            self.params.get_parameter_list(), initial_parameters=initial_parameter, rng=self._rng
+        _initial_parameters = super().generate_initial_parameter()
+        initial_parameters = np.array(
+            [
+                [
+                    self._generate_initial_parameter(_initial_parameters, dim, num_of_initials)
+                    for dim in range(self.n_params)
+                ]
+                for num_of_initials in range(self.n_dim + 1)
+            ]
         )
 
+        self.logger.debug(f"initial_parameters: {initial_parameters}")
+        if self.nelder_mead is not None:
+            return None
+        self.nelder_mead = NelderMead(initial_parameters=initial_parameters)
         return self.generate_parameter()
-
-    def check_result(self) -> None:
-        pass
-
-    def get_ready_parameters(self) -> list[Any]:
-        """Get the list of ready parameters.
-
-        Returns:
-            list
-        """
-        return self.nelder_mead._executing
-
-    def get_nm_results(self) -> list[dict[str, str | int | list[Any] | bool]]:
-        """Get the list of Nelder-Mead result.
-
-        Returns:
-            list[dict[str, str | int | list | bool]]: Results per trial.
-        """
-        nm_results = []
-        for p in self.get_ready_parameters():
-            try:
-                index = int(p["vertex_id"])
-            except ValueError:
-                continue
-            except KeyError:
-                continue
-
-            result = self.get_any_trial_objective(index)
-
-            if result is not None:
-                nm_result = copy.copy(p)
-                nm_result["result"] = result
-                nm_results.append(nm_result)
-
-        return nm_results
-
-    def _add_result(self, nm_results: list[Any]) -> None:
-        """Add a result parameter.
-
-        Args:
-            nm_results (list):
-
-        Returns:
-            None
-        """
-        if len(nm_results) == 0 or len(self.order) == 0:
-            return
-
-        # Store results in order of HP file generation
-        order = self.order[0]
-        for nm_result in nm_results:
-            if order == nm_result["vertex_id"]:
-                self.nelder_mead.add_result_parameters(nm_result)
-                self.order.pop(0)
-                break
-
-    def update_ready_parameter_name(
-        self, pool_p: dict[str, Any], name: Any  # old_param_name  # new_param_name
-    ) -> None:
-        """Update hyperparameter's names.
-
-        Args:
-            pool_p (str): old parameter name
-            name (str): New parameter name
-
-        Returns:
-            None
-
-        Note:
-            - before::
-
-                {
-                    'vertex_id': 'CMTrNe5P8a',
-                    'parameters': [
-                        {'parameter_name': 'x1', 'value': 3.37640289751353},
-                        {'parameter_name': 'x2', 'value': 1.6556037243290205}
-                    ],
-                    'state': 'WaitExpand',
-                    'itr': 5,
-                    'index': None,
-                    'out_of_boundary': False
-                }
-
-            - after::
-
-                {
-                    'vertex_id': '000014', <---- replace to trial_id
-                    'parameters': [
-                        {'parameter_name': 'x1', 'value': 3.37640289751353},
-                        {'parameter_name': 'x2', 'value': 1.6556037243290205}
-                    ],
-                    'state': 'WaitExpand',
-                    'itr': 5,
-                    'index': None,
-                    'out_of_boundary': False
-                }
-
-        """
-        old_param_name = pool_p["vertex_id"]
-        new_param_name = name
-        for e in self.nelder_mead._executing:
-            if e["vertex_id"] == old_param_name:
-                e["vertex_id"] = new_param_name
-                break
-
-    def nelder_mead_main(self) -> list[Any] | None:
-        """Nelder Mead's main module.
-
-        Args:
-            None
-
-        Returns:
-            searched_params (list): Result of optimization.
-        """
-        searched_params = self.nelder_mead.search()
-        if searched_params is None:
-            self.logger.info("generate_parameter(): reached to max iteration.")
-            return None
-        if len(searched_params) == 0:
-            return None
-        return searched_params
-
-    def _get_all_trial_id(self) -> list[Any]:
-        """_get_all_trial_id.
-
-        Get trial_ids from DB: 'result', 'finished', 'running', 'ready'
-
-        Returns:
-            list: trial_id
-        """
-        trial_id = self.storage.trial.get_all_trial_id()
-        if trial_id is None:
-            return []
-
-        return trial_id
-
-    def _get_current_names(self) -> list[Any]:
-        """Get parameter trial_id.
-
-        Returns:
-            list: A list og parameter names in parameter_pool
-        """
-        # WARN: Always empty.
-        return [p["vertex_id"] for p in self.parameter_pool]
 
     def generate_parameter(self) -> list[dict[str, float | int | str]] | None:
         """Generate parameters.
@@ -208,26 +115,127 @@ class NelderMeadOptimizer(AbstractOptimizer):
         Raises:
             TypeError: Causes when an invalid parameter type is set.
         """
-
-        self._add_result(self.get_nm_results())
-
-        searched_params = self.nelder_mead_main()
-
-        if searched_params is None:
+        searched_params: list[Vertex] = self.nelder_mead_main()
+        for searched_param in searched_params:
+            self.single_or_multiple_trial_params.append(searched_param)
+        if len(self.single_or_multiple_trial_params) == 0:
             return None
+        new_params: Vertex = self.single_or_multiple_trial_params.pop(0)
+        self.map_trial_id_and_vertex_id[self.trial_id.integer] = new_params.id
+        new_param = self.convert_ndarray_to_parameter(new_params.coordinates)
+        return new_param
 
-        for p in searched_params:
-            if p["vertex_id"] not in self._get_all_trial_id() and p["vertex_id"] not in self._get_current_names():
-                self.parameter_pool.append(copy.copy(p))
+    def inner_loop_main_process(self) -> bool:
+        """A main loop process. This process is repeated every main loop.
 
-        if len(self.parameter_pool) == 0:
-            return None
+        Returns:
+            bool: The process succeeds or not. The main loop exits if failed.
+        """
+        self.update_each_state_count()
+        if self.check_finished():
+            return False
+        if self.all_parameters_processed():
+            return False
+        if self.all_parameters_registered():
+            return True
+        pool_size = self.get_pool_size()
+        if pool_size == 0:
+            return True
+        self.logger.info(
+            f"hp_ready: {self.hp_ready}, "
+            f"hp_running: {self.hp_running}, "
+            f"hp_finished: {self.hp_finished}, "
+            f"total: {self.config.optimize.trial_number}, "
+            f"pool_size: {pool_size}"
+        )
+        if new_params := self.generate_new_parameter():
+            if self.out_of_boundary(new_params):
+                self.logger.debug(f"out of boundary: {new_params}")
+                self.register_new_parameters(self.convert_type_by_config(new_params), state="finished")
+                objective = np.inf
+                if self.goals[0] == goal_maximize:
+                    objective = -np.inf
+                self.storage.result.set_any_trial_objective(trial_id=self.trial_id.integer, objective=[objective])
+                self.trial_id.increment()
+                self._serialize(self.trial_id.integer)
+                return True
+            self.register_new_parameters(self.convert_type_by_config(new_params))
+            self.trial_id.increment()
+            self._serialize(self.trial_id.integer)
+            return True
+        self.print_dict_state()
 
-        pool_p = self.parameter_pool.pop(0)
+        return True
 
-        new_params = self.params.to_original_repr(pool_p["parameters"])
+    def out_of_boundary(self, params: list[dict[str, float | int | str]]) -> bool:
+        for param in params:
+            if param["out_of_boundary"]:
+                return True
+        return False
 
-        self.update_ready_parameter_name(pool_p, self.trial_id.get())
-        self.order.append(self.trial_id.get())
+    def nelder_mead_main(self) -> list[Vertex]:
+        """Nelder Mead's main module.
 
-        return new_params
+        Args:
+            None
+
+        Returns:
+            searched_params (list): Result of optimization.
+        """
+
+        nm_state = self.nelder_mead.get_state()
+        if nm_state in {
+            "initialize_pending",
+            "reflect_pending",
+            "expand_pending",
+            "inside_contract_pending",
+            "outside_contract_pending",
+            "shrink_pending",
+        }:
+            new_finished = self.new_finished()
+            if len(new_finished) == self.nelder_mead.get_n_waits():
+                if len(new_finished) > 1:
+                    values = []
+                    for trial_id in new_finished:
+                        self.completed_trial_ids.append(trial_id)
+                        vertex_id = self.map_trial_id_and_vertex_id[trial_id]
+                        objective = self.storage.result.get_any_trial_objective(trial_id)[0]
+                        if self.goals[0] == goal_maximize:
+                            objective *= -1.0
+                        values.append(Value(id=vertex_id, value=objective))
+                    if nm_state == "initialize_pending":
+                        self.nelder_mead.after_initialize(values)
+                    elif nm_state == "shrink_pending":
+                        self.nelder_mead.aftter_shrink(values)
+                elif len(new_finished) == 1:
+                    trial_id = new_finished[-1]
+                    vertex_id = self.map_trial_id_and_vertex_id[trial_id]
+                    objective = self.storage.result.get_any_trial_objective(trial_id)[0]
+                    if self.goals[0] == goal_maximize:
+                        objective *= -1.0
+                    value = Value(id=vertex_id, value=objective)
+                    self.completed_trial_ids.append(trial_id)
+                    if nm_state == "reflect_pending":
+                        self.nelder_mead.after_reflect(value)
+                    elif nm_state == "expand_pending":
+                        self.nelder_mead.after_expand(value)
+                    elif nm_state == "inside_contract_pending":
+                        self.nelder_mead.after_inside_contract(value)
+                    elif nm_state == "outside_contract_pending":
+                        self.nelder_mead.after_outside_contract(value)
+        elif nm_state == "initialize":
+            ...
+        elif nm_state == "reflect":
+            ...
+        elif nm_state == "expand":
+            ...
+        elif nm_state == "inside_contract":
+            ...
+        elif nm_state == "outside_contract":
+            ...
+        elif nm_state == "shrink":
+            ...
+        else:
+            raise NotImplementedError(f"Invalid state: {nm_state}")  # not reachable
+        searched_params = self.nelder_mead.search()
+        return searched_params
--- tests/unit/optimizer_test/test_nelder_mead_optimizer.py
+++ tests/unit/optimizer_test/test_nelder_mead_optimizer.py
@@ -20,16 +20,16 @@ class TestNelderMeadOptimizer(BaseTest):
 
     def test_generate_initial_parameter(self):
         expected = [
-            {'parameter_name': 'x1', 'type': 'uniform_float', 'value': 0.74},
-            {'parameter_name': 'x2', 'type': 'uniform_float', 'value': 2.98},
-            {'parameter_name': 'x3', 'type': 'uniform_float', 'value': 3.62},
-            {'parameter_name': 'x4', 'type': 'uniform_float', 'value': 0.9},
-            {'parameter_name': 'x5', 'type': 'uniform_float', 'value': 1.99},
-            {'parameter_name': 'x6', 'type': 'uniform_float', 'value': -2.78},
-            {'parameter_name': 'x7', 'type': 'uniform_float', 'value': 1.0},
-            {'parameter_name': 'x8', 'type': 'uniform_float', 'value': 4.97},
-            {'parameter_name': 'x9', 'type': 'uniform_float', 'value': 1.98},
-            {'parameter_name': 'x10', 'type': 'uniform_float', 'value': 4.03}
+            {'parameter_name': 'x1', 'type': 'uniform_float', 'value': 0.74, 'out_of_boundary': False},
+            {'parameter_name': 'x2', 'type': 'uniform_float', 'value': 2.98, 'out_of_boundary': False},
+            {'parameter_name': 'x3', 'type': 'uniform_float', 'value': 3.62, 'out_of_boundary': False},
+            {'parameter_name': 'x4', 'type': 'uniform_float', 'value': 0.9, 'out_of_boundary': False},
+            {'parameter_name': 'x5', 'type': 'uniform_float', 'value': 1.99, 'out_of_boundary': False},
+            {'parameter_name': 'x6', 'type': 'uniform_float', 'value': -2.78, 'out_of_boundary': False},
+            {'parameter_name': 'x7', 'type': 'uniform_float', 'value': 1.0, 'out_of_boundary': False},
+            {'parameter_name': 'x8', 'type': 'uniform_float', 'value': 4.97, 'out_of_boundary': False},
+            {'parameter_name': 'x9', 'type': 'uniform_float', 'value': 1.98, 'out_of_boundary': False},
+            {'parameter_name': 'x10', 'type': 'uniform_float', 'value': 4.03, 'out_of_boundary': False}
         ]
 
         _optimizer = NelderMeadOptimizer(self.load_config_for_test(self.configs["config.json"]))
@@ -38,7 +38,9 @@ class TestNelderMeadOptimizer(BaseTest):
         self.optimizer._rng = np.random.RandomState(0)
 
         with patch.object(self.optimizer, "nelder_mead", None):
-            assert self.optimizer.generate_initial_parameter() == expected
+            initial_params = self.optimizer.generate_initial_parameter()
+            for i in range(len(expected)):
+                assert initial_params[i] == expected[i]
 
         with patch.object(self.optimizer, "nelder_mead", _nelder_mead):
             assert self.optimizer.generate_initial_parameter() is None
@@ -49,191 +51,32 @@ class TestNelderMeadOptimizer(BaseTest):
         self.optimizer.post_process()
         assert self.optimizer.pre_process() is None
 
-    def test_check_result(self, setup_result):
-        self.optimizer.pre_process()
-        self.optimizer.generate_initial_parameter()
-        setup_result(1)
-        # params = self.optimizer.nelder_mead.get_ready_parameters()
-        params = self.optimizer.get_ready_parameters()
-        print(params)
-        assert self.optimizer.check_result() is None
-
     def test_generate_parameter(
         self,
         load_test_config_org,
         setup_result
     ):
-        self.optimizer.pre_process()
-        # config = load_test_config()
         config = self.load_config_for_test(self.configs["config_nelder_mead.json"])
+        self.optimizer = NelderMeadOptimizer(config)
+        self.optimizer.pre_process()
         self.optimizer.params = ConvertedParameterConfiguration(
             HyperParameterConfiguration(config.optimize.parameters), convert_log=True, convert_int=True,
             convert_choices=True, convert_sequence=True,
         )
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(
-            self.optimizer.params.get_parameter_list(),
-            rng=rng
-        )
-        # params = self.optimizer.nelder_mead.get_ready_parameters()
-        params = self.optimizer.get_ready_parameters()
-        assert params is not None
-        setup_result(len(params))
+        self.optimizer.generate_initial_parameter()
         assert len(self.optimizer.generate_parameter()) > 0
 
-        self.optimizer.nelder_mead._max_itr = 0
-        assert self.optimizer.generate_parameter() is None
-
-        # if len(self.parameter_pool) == 0:
-        self.optimizer.nelder_mead = NelderMead(self.optimizer.params.get_parameter_list(), rng=rng)
+        self.optimizer = NelderMeadOptimizer(self.load_config_for_test(self.configs["config.json"]))
         self.optimizer.generate_initial_parameter()
         with patch.object(self.optimizer, 'nelder_mead_main', return_value=[]):
-            with patch.object(self.optimizer, 'parameter_pool', []):
+            with patch.object(self.optimizer, 'single_or_multiple_trial_params', []):
                 assert self.optimizer.generate_parameter() is None
 
-    def test_generate_parameter2(
-        self,
-        load_test_config_org,
-        setup_result
-    ):
-        self.optimizer.pre_process()
-        config = self.load_config_for_test(self.configs["config.json"])
-        self.optimizer.params = ConvertedParameterConfiguration(
-            HyperParameterConfiguration(config.optimize.parameters), convert_log=True, convert_int=True,
-            convert_choices=True, convert_sequence=True,
-        )
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(
-            self.optimizer.params.get_parameter_list(),
-            rng=rng
-        )
-        # params = self.optimizer.nelder_mead.get_ready_parameters()
-        # params = self.optimizer.get_ready_parameters()
-        params = self.optimizer.nelder_mead._executing
-        setup_result(len(params))
-        assert len(self.optimizer.generate_parameter()) > 0
-
-    def test_update_ready_parameter_name(
-        self,
-        load_test_config_org
-    ):
-        self.optimizer.pre_process()
-        config = self.load_config_for_test(self.configs["config.json"])
-        self.optimizer.params = ConvertedParameterConfiguration(
-            HyperParameterConfiguration(config.optimize.parameters), convert_log=True, convert_int=True,
-            convert_choices=True, convert_sequence=True,
-        )
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(
-            self.optimizer.params.get_parameter_list(),
-            rng=rng
-        )
-        self.optimizer.nelder_mead._executing.append({'vertex_id': '001'})
-
-        pool_p = {"vertex_id": "001"}
-        assert self.optimizer.update_ready_parameter_name(pool_p, 'new') is None
-
-        pool_p = {"vertex_id": "002"}
-        assert self.optimizer.update_ready_parameter_name(pool_p, 'new') is None
-
-    def test_get_ready_parameters(
-        self,
-        load_test_config_org
-    ):
-        self.optimizer.pre_process()
-        config = load_test_config_org()
-        config = self.load_config_for_test(self.configs["config.json"])
-        self.optimizer.params = ConvertedParameterConfiguration(
-            HyperParameterConfiguration(config.optimize.parameters), convert_log=True, convert_int=True,
-            convert_choices=True, convert_sequence=True,
-        )
-
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(
-            self.optimizer.params.get_parameter_list(),
-            rng=rng
-        )
-        assert len(self.optimizer.get_ready_parameters()) == 11
-
-    def test_get_nm_results(self):
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(self.optimizer.params.get_parameter_list(), rng=rng)
-
-        self.optimizer.get_nm_results()
-
-        expected = [
-            {
-                'vertex_id': 'abc',
-                'parameters': [{'parameter_name': 'x1', 'value': -4.87}, {'parameter_name': 'x2', 'value': -0.71}],
-                'state': 'WaitInitialize',
-                'itr': 1,
-                'index': 1,
-                'out_of_boundary': False
-            },
-            {
-                'parameters': [{'parameter_name': 'x1', 'value': -4.87}, {'parameter_name': 'x2', 'value': -0.71}],
-                'state': 'WaitInitialize',
-                'itr': 1,
-                'index': 1,
-                'out_of_boundary': False
-            }
-        ]
-
-        result_content = {'trial_id': 0, 'result': 123}
-        with patch.object(self.optimizer.nelder_mead, '_executing', expected):
-            with patch.object(self.optimizer.storage.result, 'get_any_trial_objective', return_value=result_content):
-                self.optimizer.get_nm_results()
-
-    def test__add_result(self):
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(self.optimizer.params.get_parameter_list(), rng=rng)
-        self.optimizer.generate_initial_parameter()
-        nm_results = [
-            {
-                'vertex_id': '0001',
-                'parameters': [{'parameter_name': 'x1', 'value': -4.87}, {'parameter_name': 'x2', 'value': -0.71}],
-                'state': 'WaitInitialize',
-                'itr': 1,
-                'index': 1,
-                'out_of_boundary': False
-            },
-        ]
-        order = [
-            {
-                'vertex_id': '0001',
-                'parameters': [{'parameter_name': 'x1', 'value': -4.87}, {'parameter_name': 'x2', 'value': -0.71}]
-            }
-        ]
-        order2 = [
-            {
-                'vertex_id': 'invalid',
-                'parameters': [{'parameter_name': 'x1', 'value': -4.87}, {'parameter_name': 'x2', 'value': -0.71}]
-            }
-        ]
-        assert self.optimizer._add_result(nm_results) is None
-
-        with patch.object(self.optimizer, 'order', order):
-            assert self.optimizer._add_result(nm_results) is None
-
-        with patch.object(self.optimizer, 'order', order2):
-            assert self.optimizer._add_result(nm_results) is None
-
     def test_nelder_mead_main(self):
-        rng = np.random.RandomState(0)
-        self.optimizer.nelder_mead = NelderMead(self.optimizer.params.get_parameter_list(), rng=rng)
+        config = self.load_config_for_test(self.configs["config_nelder_mead.json"])
+        self.optimizer = NelderMeadOptimizer(config)
         self.optimizer.generate_initial_parameter()
         self.optimizer.nelder_mead_main()
 
-        with patch.object(self.optimizer.nelder_mead, 'search', return_value=None):
-            assert self.optimizer.nelder_mead_main() is None
-
         with patch.object(self.optimizer.nelder_mead, 'search', return_value=[]):
-            assert self.optimizer.nelder_mead_main() is None
-
-    def test__get_all_trial_id(self):
-        with patch.object(self.optimizer.storage.trial, 'get_all_trial_id', return_value=None):
-            assert self.optimizer._get_all_trial_id() == []
-
-        expected = [1, 2, 3]
-        with patch.object(self.optimizer.storage.trial, 'get_all_trial_id', return_value=expected):
-            assert self.optimizer._get_all_trial_id() == expected
+            assert self.optimizer.nelder_mead_main() == []
