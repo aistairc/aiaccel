05563129b0688e1e729cc6af9160c017951a3740
Takashi OKADA <tk.okada.github@gmail.com>
2023-12-05 17:06:22 +0900

    update

--- aiaccel/nas/batch_dependent_lr_scheduler/__init__.py
+++ aiaccel/nas/batch_dependent_lr_scheduler/__init__.py
@@ -1,19 +1,19 @@
-from nas.batch_dependent_lr_scheduler.abstract_batch_dependent_lr_shceduler import (
+from aiaccel.nas.batch_dependent_lr_scheduler.abstract_batch_dependent_lr_shceduler import (
     AbstractBatchDependentLRScheduler,
 )
-from nas.batch_dependent_lr_scheduler.batch_dependent_cosine_lr_scheduler import (
+from aiaccel.nas.batch_dependent_lr_scheduler.batch_dependent_cosine_lr_scheduler import (
     BatchDependentCosineLRScheduler,
 )
-from nas.batch_dependent_lr_scheduler.batch_dependent_exponential_lr_scheduler import (
+from aiaccel.nas.batch_dependent_lr_scheduler.batch_dependent_exponential_lr_scheduler import (
     BatchDependentExponentialLRScheduler,
 )
-from nas.batch_dependent_lr_scheduler.batch_dependent_linear_lr_scheduler import (
+from aiaccel.nas.batch_dependent_lr_scheduler.batch_dependent_linear_lr_scheduler import (
     BatchDependentLinearLRScheduler,
 )
-from nas.batch_dependent_lr_scheduler.batch_dependent_multi_step_lr_scheduler import (
+from aiaccel.nas.batch_dependent_lr_scheduler.batch_dependent_multi_step_lr_scheduler import (
     BatchDependentMultiStepLRScheduler,
 )
-from nas.batch_dependent_lr_scheduler.create import create_batch_dependent_lr_scheduler
+from aiaccel.nas.batch_dependent_lr_scheduler.create import create_batch_dependent_lr_scheduler
 
 __all__ = [
     "AbstractBatchDependentLRScheduler",
