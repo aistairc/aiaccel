2df1bd4e074b85286ef0908e47aff7538fe47f37
Takashi OKADA <tk.okada.github@gmail.com>
2023-12-14 08:37:24 +0900

    update

--- aiaccel/nas/__init__.py
+++ aiaccel/nas/__init__.py
@@ -1,9 +1,8 @@
-from aiaccel import asng, batch_dependent_lr_scheduler, data_module, module, nas_model, trainer, utils
+from aiaccel.nas import asng, batch_dependent_lr_scheduler, data_module, module, nas_model, trainer, utils
 from aiaccel.nas.create_optimizer import create_optimizer
 from aiaccel.nas.mnas_structure_info import MnasStructureInfo
 
 __all__ = [
-    "asng",
     "asng",
     "batch_dependent_lr_scheduler",
     "data_module",
