3166a64bead68c1dbeb4f7e06db979a596cc7a4a
KanaiYuma-aist <105629713+KanaiYuma-aist@users.noreply.github.com>
2024-03-04 09:37:25 +0900

    Update aiaccel/hpo/samplers/nelder_mead_sampler.py
    Co-authored-by: Yoshiaki Bando <yoshipon@users.noreply.github.com>

--- aiaccel/hpo/samplers/nelder_mead_sampler.py
+++ aiaccel/hpo/samplers/nelder_mead_sampler.py
@@ -28,7 +28,7 @@ class NelderMeadAlgorism:
 
     def __init__(
         self,
-        search_space: dict[str, list[float]],
+        search_space: dict[str, tuple[float, float]],
         coeff: NelderMeadCoefficient | None = None,
         rng: np.random.RandomState | None = None,
         block: bool = True,
