2eaf9d7e198ccb77eb3331404c07f489ace44d5b
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2024-04-07 22:36:25 +0900

    Improve configs for readability

--- tests/unit_tests/test_aiaccel.py
+++ tests/unit_tests/test_aiaccel.py
@@ -3,4 +3,4 @@ import unittest
 
 class TestAiaccel(unittest.TestCase):
     def test_aiaccel(self) -> None:
-        assert True # TODO: implement your test here
+        assert True  # TODO: implement your test here
