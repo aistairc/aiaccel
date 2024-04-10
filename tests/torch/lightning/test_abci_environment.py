fe51c6aeb36e546810416f9e8ae9238ceb8fadb1
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2024-04-11 00:24:52 +0900

    Add several tests

---
+++ tests/torch/lightning/test_abci_environment.py
@@ -0,0 +1,30 @@
+import os
+from unittest import mock
+
+from aiaccel.torch.lightning.abci_environment import ABCIEnvironment
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "OMPI_COMM_WORLD_SIZE": "8",
+        "OMPI_COMM_WORLD_RANK": "6",
+        "OMPI_COMM_WORLD_LOCAL_RANK": "2",
+        "OMPI_COMM_WORLD_LOCAL_SIZE": "4",
+        "MAIN_ADDR": "192.168.0.1",
+        "MAIN_PORT": "3000",
+    },
+)
+def test_abci_environment() -> None:
+    env = ABCIEnvironment()
+
+    assert env.creates_processes_externally
+
+    assert env.main_address == "192.168.0.1"
+    assert env.main_port == 3000
+
+    assert env.detect()
+    assert env.world_size() == 8
+    assert env.global_rank() == 6
+    assert env.node_rank() == 1
+    assert env.local_rank() == 2
