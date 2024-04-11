ced8ff5c9b64382470d6b4d2bd62f02a634f73d4
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2024-04-12 00:18:36 +0900

    update train.py

--- aiaccel/apps/train.py
+++ aiaccel/apps/train.py
@@ -1,46 +1,51 @@
 from argparse import ArgumentParser
-import os
 from pathlib import Path
 import pickle as pkl
 
 from hydra.utils import instantiate
+from omegaconf import DictConfig, ListConfig
 from omegaconf import OmegaConf as oc
 
-import lightning as pl
+import lightning as lt
+from lightning.fabric.utilities.rank_zero import rank_zero_only
 
 from aiaccel.utils import print_config
 
 
+@rank_zero_only
+def print_and_save_config(config: DictConfig | ListConfig) -> None:
+    print_config(config)
+
+    with open(config.working_directory / "config.pkl", "wb") as f:
+        pkl.dump(config, f)
+
+
 def main() -> None:
     parser = ArgumentParser()
-    parser.add_argument(
-        "config",
-        type=str,
-        help="Config file in YAML format",
-    )
-    parser.add_argument(
-        "--working_directory",
-        type=str,
-        default=str(Path.cwd()),  # todo: treat path as Path
-        help="Working directory",
-    )
+    parser.add_argument("config", type=Path, help="Config file in YAML format")
+    parser.add_argument("--working_directory", type=Path, default=Path.cwd(), help="Working directory")
     args, unk_args = parser.parse_known_args()
 
     # load config
     config = oc.merge(
-        oc.load(args.config),
+        {
+            "base_config_path": str(Path(__file__).parent / "config"),
+            "base_config": "${base_config_path}/train_base.yaml",
+        },
         vars(args),
+        oc.load(args.config),
         oc.from_cli(unk_args),
     )
 
-    if "OMPI_COMM_WORLD_RANK" not in os.environ or int(os.environ["OMPI_COMM_WORLD_RANK"]) == 0:
-        print_config(config)
+    config = oc.merge(
+        oc.load(config.base_config),
+        config,
+    )
 
-        with open(Path(config.working_directory) / "config.pkl", "wb") as f:
-            pkl.dump(config, f)
+    print_and_save_config(config)
 
     # train
-    trainer: pl.Trainer = instantiate(config.trainer)
+    trainer: lt.Trainer = instantiate(config.trainer)
     trainer.fit(
         model=instantiate(config.task),
         datamodule=instantiate(config.datamodule),
