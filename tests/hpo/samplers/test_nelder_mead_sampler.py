eb5e1363e95c190be4e3068f8c3872b3c0366fa6
KanaiYuma <kanai-itc1013@aist.go.jp>
2024-04-12 16:27:12 +0900

    Fix code format.

--- tests/hpo/samplers/test_nelder_mead_sampler.py
+++ tests/hpo/samplers/test_nelder_mead_sampler.py
@@ -91,7 +91,7 @@ class TestNelderMeadSampler(unittest.TestCase):
 def ackley(x: list[float]) -> float:
     # Ackley function
     y = (
-        -20 * np.exp(-0.2 * np.sqrt(0.5 * (x[0]**2 + x[1]**2)))
+        -20 * np.exp(-0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2)))
         - np.exp(0.5 * (np.cos(2 * np.pi * x[0]) + np.cos(2 * np.pi * x[1])))
         + np.e
         + 20
@@ -104,7 +104,7 @@ def ackley_sleep(x: list[float]) -> float:
     time.sleep(np.random.uniform(0.001, 0.01))
     # Ackley function
     y = (
-        -20 * np.exp(-0.2 * np.sqrt(0.5 * (x[0]**2 + x[1]**2)))
+        -20 * np.exp(-0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2)))
         - np.exp(0.5 * (np.cos(2 * np.pi * x[0]) + np.cos(2 * np.pi * x[1])))
         + np.e
         + 20
