202fcea9d5fa50351c0b046377522ac4a38bfc53
aramoto99 <aramoto.masahiro@gmail.com>
2024-04-18 15:54:41 +0900

    Reflected code review feedback

--- aiaccel/job/dispatcher.py
+++ aiaccel/job/dispatcher.py
@@ -60,23 +60,23 @@ class AbciJob:
         output = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         pattern = r"Your job (\d+)"
         match = re.search(pattern, output.stdout)
-
         if match:
             self.job_number = int(match.group(1))
         else:
             raise ValueError(f"Failed to submit the job: {self.job_name}")
 
         for _ in range(10):
-            self.update_state()
-            if self.status != JobStatus.UNSUBMITTED:
-                break
+            job_status = self.get_job_status(self.job_number)
+            if job_status is not None:
+                self.status = status_mapping[job_status]
+                print(f"current status: {self.status}")
+                return
             time.sleep(3)
             # wait for the job to be submitted
+        else:
+            raise ValueError(f"Failed to get the job status: {self.job_name}")
 
-        if self.status == JobStatus.UNSUBMITTED:
-            raise ValueError(f"Failed to submit the job: {self.job_name}")
-
-    def _get_job_status(self, job_number: int) -> str | None:
+    def get_job_status(self, job_number: int) -> str | None:
         """
         Get the status of the job.
         """
@@ -89,26 +89,15 @@ class AbciJob:
             if int(job_list["JB_job_number"]) == job_number:
                 return str(job_list["state"])
             return None
-
         for job_list in qstat["job_info"]["queue_info"]["job_list"]:
             if int(job_list["JB_job_number"]) == job_number:
                 return str(job_list["state"])
         return None
 
-    def update_state(self) -> None:
-        if self.job_number is None:
-            raise ValueError("Job number is not set.")
-        status = self._get_job_status(self.job_number)
-        if self.status != JobStatus.UNSUBMITTED and status is None:
-            self.status = JobStatus.FINISHED
-        else:
-            if status is not None:
-                self.status = status_mapping[status]
-
     def set_state(self, state: JobStatus) -> None:
         self.status = state
 
-    def collect_result(self, retry_left: int = 10, wait: float = 1.0) -> str:
+    def collect_result(self, retry_left: int = 10, wait: float = 1.0) -> str | None:
         """Collect the result of the job.
 
         return:
@@ -125,17 +114,22 @@ class AbciJob:
                     if lines:
                         return lines[-1].strip()
                     else:
-                        raise ValueError("No result found.")
-
-            except FileNotFoundError:
+                        raise ValueError(
+                            f"No result found in `{self.stdout_file_path}`."
+                        )
+            except FileNotFoundError as e:
                 time.sleep(wait)
                 retry_left -= 1
-
-            except ValueError:
+                if retry_left == 0:
+                    raise FileNotFoundError(
+                        f"result file not found in `{self.stdout_file_path}`."
+                    )
+            except ValueError as e:
                 time.sleep(wait)
                 retry_left -= 1
-
-        raise ValueError("No result found.")
+                if retry_left == 0:
+                    raise ValueError(f"No result found in `{self.stdout_file_path}`.")
+        return None
 
     def get_result(self) -> Any:
         y = self.collect_result()
@@ -157,19 +151,21 @@ class AbciJobExecutor:
         self.work_dir: Path = Path(work_dir).resolve()
         if not self.work_dir.exists():
             self.work_dir.mkdir(parents=True)
-        self._submit_job_count: int = 0
-        self._finished_job_count: int = 0
+        self.submit_job_count: int = 0
+        self.finished_job_count: int = 0
 
-    def submit(self, args: list[str], group: str, job_name: str = "", tag: Any | None = None) -> AbciJob:
+    def submit(
+        self, args: list[str], group: str, job_name: str = "", tag: Any | None = None
+    ) -> AbciJob:
         if job_name == "":
             job_name = str(uuid.uuid4())
         stdout_file_path = self.work_dir / f"{job_name}.o"
         stderr_file_path = self.work_dir / f"{job_name}.e"
         args_str = " ".join(args)
-        qsub_cmd = (
-            f"qsub -g {group} -N {job_name} -o {stdout_file_path} -e {stderr_file_path} {self.job_file_path} {args_str}"
+        qsub_cmd = f"qsub -g {group} -N {job_name} -o {stdout_file_path} -e {stderr_file_path} {self.job_file_path} {args_str}"
+        return self._execute(
+            qsub_cmd, job_name, args, tag, stdout_file_path, stderr_file_path
         )
-        return self._execute(qsub_cmd, job_name, args, tag, stdout_file_path, stderr_file_path)
 
     def _execute(
         self,
@@ -180,7 +176,7 @@ class AbciJobExecutor:
         stdout_file_path: Path,
         stderr_file_path: Path,
     ) -> AbciJob:
-        self._submit_job_count += 1
+        self.submit_job_count += 1
 
         job = AbciJob(
             job_name,
@@ -197,7 +193,7 @@ class AbciJobExecutor:
         # Wait for at least one available worker
         while True:
             self.update_state_batch()
-            if self.available_worker_count > 0:
+            if self.get_available_worker_count() > 0:
                 break
             time.sleep(3)
         return job
@@ -207,34 +203,20 @@ class AbciJobExecutor:
         for job in self.working_job_list:
             if job.status == JobStatus.FINISHED:
                 self.working_job_list.pop(self.working_job_list.index(job))
-                self._finished_job_count += 1
+                self.finished_job_count += 1
                 yield job.get_result(), job.tag
 
     def get_result(self) -> Any:
         self.update_state_batch()
         if self.working_job_list[-1].status == JobStatus.FINISHED:
             job = self.working_job_list.pop(0)
-            self._finished_job_count += 1
+            self.finished_job_count += 1
             return job.get_result()
         else:
             return None
 
-    def update_state(self, job_number: int, state: str) -> None:
-        for job in self.working_job_list:
-            if job.job_number == job_number:
-                job.set_state(status_mapping[state])
-                break
-            job.set_state(JobStatus.FINISHED)
-
     @classmethod
     def update_state_batch(cls) -> None:
-        def update_state(job_number: int, state: str) -> None:
-            for job in cls.working_job_list:
-                if job.job_number == job_number:
-                    job.set_state(status_mapping[state])
-                    break
-                job.set_state(JobStatus.FINISHED)
-
         qstat = qstat_xml()
         queue_info = qstat["job_info"].get("queue_info", None)
         job_info = qstat["job_info"].get("job_info", None)
@@ -244,30 +226,51 @@ class AbciJobExecutor:
         elif job_info:
             job_list = job_info.get("job_list", None)
         else:
+            # No job is running, so all jobs are finished
             for working_job in cls.working_job_list:
-                working_job.set_state(JobStatus.FINISHED)
+                if working_job.status not in [
+                    JobStatus.FINISHED,
+                    JobStatus.ERROR,
+                    JobStatus.UNSUBMITTED,
+                ]:
+                    working_job.set_state(JobStatus.FINISHED)
             return
 
+        def update_state(job_list: list[dict[str, str]]) -> None:
+            qstat_job_numbers = [int(job["JB_job_number"]) for job in job_list]
+            working_job_numbers = [job.job_number for job in cls.working_job_list]
+            seen_job_numbers = set()
+
+            # qstat結果に存在しないジョブは終了したと判断
+            for job_number in set(working_job_numbers) - set(qstat_job_numbers):
+                for job in cls.working_job_list:
+                    if job.job_number == job_number:
+                        job.set_state(JobStatus.FINISHED)
+                        seen_job_numbers.add(job_number)
+
+            for job_dict in job_list:
+                job_number = int(job_dict["JB_job_number"])
+                new_state = job_dict["state"]
+                if job_number in seen_job_numbers:
+                    continue
+                for working_job in cls.working_job_list:
+                    if working_job.job_number == job_number:
+                        if working_job.status == status_mapping[new_state]:
+                            continue
+                        working_job.set_state(status_mapping[new_state])
+                        seen_job_numbers.add(job_number)
+                        break
+
         if isinstance(job_list, dict):
-            update_state(int(job_list["JB_job_number"]), job_list["state"])
-        elif isinstance(job_list, list):
-            for job in job_list:
-                update_state(int(job["JB_job_number"]), job["state"])
+            update_state([job_list])
         else:
-            raise NotImplementedError("Unexpected job_list type.")
+            update_state(job_list)
 
-    @property
-    def available_worker_count(self) -> int:
-        _working_job_count = len([j for j in self.working_job_list if j.status != JobStatus.FINISHED])
+    def get_available_worker_count(self) -> int:
+        _working_job_count = len(
+            [j for j in self.working_job_list if j.status != JobStatus.FINISHED]
+        )
         return self._n_jobs - _working_job_count
-
-    @property
-    def finished_job_count(self) -> int:
-        return self._finished_job_count
-
-    @property
-    def submit_job_count(self) -> int:
-        return self._submit_job_count
 
     @property
     def job_list(self) -> list[AbciJob]:
--- tests/job/dispatcher/test_dispatcher.py
+++ tests/job/dispatcher/test_dispatcher.py
@@ -3,12 +3,66 @@ from __future__ import annotations
 import shutil
 import unittest
 from pathlib import Path
+from unittest.mock import patch
+from typing import Any
+
+import pytest
+
+from aiaccel.job.dispatcher import AbciJob, AbciJobExecutor, JobStatus
+
+
+class Output:
+    stdout = 'Your job 42183931 ("hpo-0006") has been submitted'
+    stderr = ""
+
+
+# queue_info: 2 jobs
+# job_info  : None
+
+qstat_xml_dict = {
+    "job_info": {
+        "queue_info": {
+            "job_list": [
+                {
+                    "@state": "running",
+                    "JB_job_number": "42183931",
+                    "JAT_prio": "0.25586",
+                    "JB_name": "hpo-0006",
+                    "JB_owner": "owner",
+                    "state": "r",
+                    "JAT_start_time": "2024-03-26T09:49:16.592",
+                    "queue_name": "gpu@g0007",
+                    "jclass_name": None,
+                    "slots": "10",
+                },
+                {
+                    "@state": "running",
+                    "JB_job_number": "42183935",
+                    "JAT_prio": "0.25586",
+                    "JB_name": "hpo-0007",
+                    "JB_owner": "owner",
+                    "state": "r",
+                    "JAT_start_time": "2024-03-26T09:49:36.770",
+                    "queue_name": "gpu@g0007",
+                    "jclass_name": None,
+                    "slots": "10",
+                },
+            ]
+        },
+        "job_info": None,
+    }
+}
 
 
 class TestAbciJob(unittest.TestCase):
-    def test___init__(self) -> None:
-        from aiaccel.job.dispatcher import AbciJob
 
+    @pytest.fixture
+    def tmpdir_fixture(self, tmpdir):  # type: ignore
+        tmpdir.mkdir("test_work")
+        self.work_dir = tmpdir / "test_work"
+        return tmpdir
+
+    def test___init__(self) -> None:
         job_file_path = Path("job.sh")
         work_dir = Path("./test_work").resolve()
         job = AbciJob(
@@ -23,88 +77,238 @@ class TestAbciJob(unittest.TestCase):
         if work_dir.exists():
             shutil.rmtree(work_dir)
 
+    def test_run(self) -> None:
+        with patch("subprocess.run", return_value=Output()) as mock_run:
+            job_file_path = Path("job.sh")
+            job = AbciJob(
+                job_name="test",
+                args=[],
+                tag=None,
+                job_file_path=job_file_path,
+                stdout_file_path=Path("stdout"),
+                stderr_file_path=Path("stderr"),
+            )
+            with patch("aiaccel.job.dispatcher.qstat_xml", return_value=qstat_xml_dict):
+                job.run("qsub -g test -N test -o stdout -e stderr job.sh")
 
-class TestAbciJobExecutor(unittest.TestCase):
-    def test___init__(self) -> None:
-        from aiaccel.job.dispatcher import AbciJobExecutor
+    def test_get_job_status(self) -> None:
+        test_job_nummber = 42183931
+        expected_state = "r"
+        job_file_path = Path("job.sh")
+        job = AbciJob(
+            job_name="test",
+            args=[],
+            tag=None,
+            job_file_path=job_file_path,
+            stdout_file_path=Path("stdout"),
+            stderr_file_path=Path("stderr"),
+        )
+        with patch("aiaccel.job.dispatcher.qstat_xml", return_value=qstat_xml_dict):
+            assert job.get_job_status(test_job_nummber) == expected_state
 
-        job_file_path = "job.sh"
-        n_jobs = 1
-        work_dir = Path("./test_work").resolve()
-        dispatcher = AbciJobExecutor(job_file_path, n_jobs, work_dir=str(work_dir))
-        self.assertEqual(dispatcher.__class__.__name__, "AbciJobExecutor")
-        if work_dir.exists():
-            shutil.rmtree(work_dir)
+    def test_set_state(self) -> None:
+        job_file_path = Path("job.sh")
+        job = AbciJob(
+            job_name="test",
+            args=[],
+            tag=None,
+            job_file_path=job_file_path,
+            stdout_file_path=Path("stdout"),
+            stderr_file_path=Path("stderr"),
+        )
 
-    # @patch("aiaccel.job.dispatcher.subprocess.run")
-    # def test_submit(self, mock_run) -> None:
-    #     from aiaccel.job.dispatcher import AbciJobExecutor
-    #     job_file_path = "job.sh"
-    #     n_jobs = 1
-    #     work_dir = Path("./test_work").resolve()
+        job.set_state(JobStatus.RUNNING)
+        assert job.status == JobStatus.RUNNING
 
-    #     mock_stdout = MagicMock()
-    #     mock_stdout.configure_mock(
-    #         **{
-    #             "stdout": "0.12",
-    #             "stderr": "",
-    #             "returncode": 0,
-    #         }
-    #     )
-
-    #     dispatcher = AbciJobExecutor(job_file_path, n_jobs, work_dir=str(work_dir))
-    #     args = ["--x1", "0.0", "--x2", "0.0"]
-
-    #     with patch("aiaccel.job.dispatcher.subprocess.run", return_value=mock_stdout):
-    #         assert dispatcher.submit(args, job_name="test") is None
-
-    def test_sibmit(self) -> None:
-        pass
-
-    def test_qsub(self) -> None:
-        pass
-
-    def test__execute_qsub(self) -> None:
-        pass
-
-    def test_get_results(self) -> None:
-        pass
+    @pytest.mark.usefixtures("tmpdir_fixture")
+    def test_collect_result(self) -> None:
+        stdout_file_path = self.work_dir / "test.o"
+        stderr_file_path = self.work_dir / "test.e"
+        job = AbciJob(
+            job_name="test",
+            args=[],
+            tag=None,
+            job_file_path=Path("job.sh"),
+            stdout_file_path=stdout_file_path,
+            stderr_file_path=stderr_file_path,
+        )
+        with open(stdout_file_path, "w") as f:
+            f.write("0.12")
+        assert job.collect_result() == "0.12"
 
     def test_get_result(self) -> None:
+        job_file_path = Path("job.sh")
+        job = AbciJob(
+            job_name="test",
+            args=[],
+            tag=None,
+            job_file_path=job_file_path,
+            stdout_file_path=Path("stdout"),
+            stderr_file_path=Path("stderr"),
+        )
+        with patch(
+            "aiaccel.job.dispatcher.AbciJob.collect_result", return_value="0.12"
+        ):
+            assert job.get_result() == "0.12"
+
+
+class TestAbciJobExecutor(unittest.TestCase):
+    job_file_path = "job.sh"
+    n_jobs = 4
+    work_dir = None
+
+    @pytest.fixture
+    def tmpdir_fixture(self, tmpdir):  # type: ignore
+        tmpdir.mkdir("test_work")
+        self.work_dir = tmpdir / "test_work"
+        return tmpdir
+
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test___init__(self) -> None:
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=str(self.work_dir)
+        )
+        self.assertEqual(dispatcher.__class__.__name__, "AbciJobExecutor")
+
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test_submit(self) -> None:
+        args = ["--x1", "0.0", "--x2", "0.0"]
+        job_name = "test"
+        group = "test_group"
+        tag = None
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=str(self.work_dir)
+        )
+        job = AbciJob(
+            job_name=job_name,
+            args=args,
+            tag=tag,
+            job_file_path=Path("job.sh"),
+            stdout_file_path=Path("stdout"),
+            stderr_file_path=Path("stderr"),
+        )
+        with patch("aiaccel.job.dispatcher.AbciJobExecutor._execute", return_value=job):
+            assert dispatcher.submit(args, group, job_name, tag) is job
+
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test_execute(self) -> None:
         pass
 
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test_get_results(self) -> None:
+        JB_name = "hpo-0006"
+        stdout_file_path = self.work_dir / f"{JB_name}.o"
+        stderr_file_path = self.work_dir / f"{JB_name}.e"
+
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=self.work_dir
+        )
+        job = AbciJob(
+            job_name=JB_name,
+            args=[],
+            tag=None,
+            job_file_path=Path("job.sh"),
+            stdout_file_path=stdout_file_path,
+            stderr_file_path=stderr_file_path,
+        )
+
+        job.job_number = 42183931
+        job.status = JobStatus.FINISHED
+        dispatcher.working_job_list.append(job)
+
+        with open(stdout_file_path, "w") as f:
+            f.write("0.12")
+
+        qstat_xml_dict = {"job_info": {"queue_info": None, "job_info": None}}
+        with patch("aiaccel.job.dispatcher.qstat_xml", return_value=qstat_xml_dict):
+            with patch(
+                "aiaccel.job.dispatcher.AbciJob.collect_result", return_value="0.12"
+            ):
+                for y, trial in dispatcher.get_results():
+                    assert y == "0.12"
+
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test_get_result(self) -> None:
+        JB_name = "hpo-0006"
+        stdout_file_path = self.work_dir / f"{JB_name}.o"
+        stderr_file_path = self.work_dir / f"{JB_name}.e"
+
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=self.work_dir
+        )
+        job = AbciJob(
+            job_name=JB_name,
+            args=[],
+            tag=None,
+            job_file_path=Path("job.sh"),
+            stdout_file_path=stdout_file_path,
+            stderr_file_path=stderr_file_path,
+        )
+
+        job.job_number = 42183931
+        job.status = JobStatus.FINISHED
+        dispatcher.working_job_list.append(job)
+
+        with open(stdout_file_path, "w") as f:
+            f.write("0.12")
+
+        qstat_xml_dict = {"job_info": {"queue_info": None, "job_info": None}}
+        with patch("aiaccel.job.dispatcher.qstat_xml", return_value=qstat_xml_dict):
+            with patch(
+                "aiaccel.job.dispatcher.AbciJob.collect_result", return_value="0.12"
+            ):
+                y = dispatcher.get_result()
+                assert y == "0.12"
+
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
     def test_update_state_batch(self) -> None:
-        pass
+        JB_names = ["hpo-0006", "hpo-0007"]
+        job_numbers = [42183931, 42183935]
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=self.work_dir
+        )
 
-    def test_available_worker_count(self) -> None:
-        pass
+        for JB_name, job_number in zip(JB_names, job_numbers):
+            stdout_file_path = self.work_dir / f"{JB_name}.o"
+            stderr_file_path = self.work_dir / f"{JB_name}.e"
 
-    def test_finished_job_count(self) -> None:
-        pass
+            job = AbciJob(
+                job_name=JB_name,
+                args=[],
+                tag=None,
+                job_file_path=Path("job.sh"),
+                stdout_file_path=stdout_file_path,
+                stderr_file_path=stderr_file_path,
+            )
+            job.job_number = job_number
+            job.set_state(JobStatus.RUNNING)
+            dispatcher.working_job_list.append(job)
 
-    def test_submit_job_count(self) -> None:
-        pass
+        qstat_xml_dict = {"job_info": {"queue_info": None, "job_info": None}}
 
+        with patch("aiaccel.job.dispatcher.qstat_xml", return_value=qstat_xml_dict):
+            dispatcher.update_state_batch()
+            for job in dispatcher.working_job_list:
+                assert job.status == JobStatus.FINISHED
 
-def test_create_run_command() -> None:
-    pass
-
-
-def test_create_qsub_command() -> None:
-    pass
-
-
-def test_collect_result() -> None:
-    pass
-
-
-def test_get_job_number() -> None:
-    pass
-
-
-def test_get_job_numbers() -> None:
-    pass
-
-
-def test_get_job_status() -> None:
-    pass
+    @pytest.mark.usefixtures("tmpdir_fixture")  # type: ignore
+    def test_get_available_worker_count(self) -> None:
+        dispatcher = AbciJobExecutor(
+            self.job_file_path, self.n_jobs, work_dir=str(self.work_dir)
+        )
+        for i in range(10):
+            dispatcher.working_job_list.append(
+                AbciJob(
+                    job_name=f"test_job_{i}",
+                    args=[],
+                    tag=None,
+                    job_file_path=Path("job.sh"),
+                    stdout_file_path=Path("stdout"),
+                    stderr_file_path=Path("stderr"),
+                )
+            )
+        for i in range(0, 7):
+            dispatcher.working_job_list[i].status = JobStatus.FINISHED
+        for i in range(7, 10):
+            dispatcher.working_job_list[i].status = JobStatus.RUNNING
+        assert dispatcher.get_available_worker_count() == 1
