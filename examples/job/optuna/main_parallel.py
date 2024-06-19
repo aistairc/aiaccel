b8c0b1bb5870a9b5c11dd7170164b24bedfc0b29
aramoto99 <aramoto-masahiro@aist.go.jp>
2024-06-19 18:43:24 +0900

    Add Job dispatcher (#360)
    * Update aiaccel/job
    
    * fix lint error
    
    * fix aiaccel/job/dispatcher.py
    
    * wip
    
    * Fix issues pointed out in review
    
    * rename
    
    * fix test
    
    * fix for test
    
    * fix for test
    
    * fix for test
    
    * fix for test
    
    * Reflected code review feedback
    
    * fix test
    
    * fix test
    
    * refactoring
    
    * Run code formatter ruff
    
    * refactoring
    
    * Run code formatter ruff
    
    * fix lint.yaml
    
    * fix pypoject.toml
    
    * fix mypy.ini
    
    * fix mypy.ini
    
    * fix mypy.ini
    
    * fix mypy.ini
    
    * fix mypy.ini
    
    * Refactor abci jobs
    
    * Update pyproject.toml
    
    * adjusted to work in the ABCI environment.
    
    * fix lint error
    
    * fix test
    
    * fix test
    
    * Changed from unittest to pytest
    
    * Incorporate revisions based on review feedback
    
    * Add test items
    
    * fix test
    
    * fix test
    
    * fix test
    
    * remove __future__.annotations
    
    * A bit simplify qstat_xml
    
    * Simplify tests
    
    ---------
    
    Co-authored-by: Yoshiaki Bando <yoshipon@users.noreply.github.com>

---
+++ examples/job/optuna/main_parallel.py
@@ -0,0 +1,50 @@
+import pickle as pkl
+from pathlib import Path
+
+import optuna
+
+from aiaccel.job import AbciJobExecutor
+
+
+def main() -> None:
+    n_trials = 50
+
+    job_filename = Path("objective.sh")
+    job_group = "gaa50000"
+
+    result_filename_template = "{job.cwd}/{job.job_name}_result.pkl"
+
+    jobs = AbciJobExecutor(job_filename, job_group)
+
+    study = optuna.create_study(direction="minimize")
+
+    finished_job_count = 0
+
+    while finished_job_count <= n_trials:
+        for _ in range(jobs.available_slots()):
+            trial = study.ask()
+            hparams = {
+                "x1": trial.suggest_float("x1", 0, 10),
+                "x2": trial.suggest_float("x2", 0, 10),
+            }
+
+            jobs.job_name = str(jobs.job_filename) + f"_{trial.number}"
+
+            job = jobs.submit(
+                args=[result_filename_template] + sum([[f"--{k}", f"{v:.5f}"] for k, v in hparams.items()], []),
+                tag=trial,
+            )
+
+        for job in jobs.collect_finished():
+            trial = job.tag
+
+            with open(result_filename_template.format(job=job), "rb") as f:
+                y = pkl.load(f)
+
+            study.tell(trial, y)
+
+            finished_job_count += 1
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/job/test_abci_job_manager.py
@@ -0,0 +1,120 @@
+import shutil
+from collections.abc import Generator
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import pytest
+from utils import qstat_xml
+
+from aiaccel.job import AbciJob, AbciJobExecutor, JobStatus
+
+
+@pytest.fixture
+def executor(tmpdir: Path) -> Generator[AbciJobExecutor, None, None]:
+    work_dir = tmpdir / "workdir"
+    work_dir.mkdir()
+
+    n_max_jobs = 4
+
+    yield AbciJobExecutor(
+        job_filename="main.sh",
+        job_group="group",
+        job_name="job",
+        work_dir=str(work_dir),
+        n_max_jobs=n_max_jobs,
+    )
+
+    shutil.rmtree(str(work_dir))
+
+
+def test_available_slots_full(executor: AbciJobExecutor) -> None:
+    """
+    - Job status:
+        - RUNNING: 4
+        - available: 0
+    """
+
+    for ii in range(4):
+        job = AbciJob(executor.job_filename, executor.job_group, job_name=executor.job_name)
+        job.status = JobStatus.RUNNING
+        job.job_number = 42340793 + 2 * ii
+
+        executor.job_list.append(job)
+
+    with patch("subprocess.run", return_value=qstat_xml("tests/job/qstat_dat.txt")):
+        slots = executor.available_slots()
+        assert slots == executor.n_max_jobs - len([j for j in executor.job_list if j.status <= JobStatus.RUNNING])
+
+
+def test_available_slots_pending(executor: AbciJobExecutor) -> None:
+    """
+    - Job status:
+        - RUNNING: 2
+        - WAITING: 1
+    - available: 1
+    """
+
+    for ii, status in enumerate([JobStatus.RUNNING, JobStatus.RUNNING, JobStatus.WAITING]):
+        job = AbciJob(executor.job_filename, executor.job_group, job_name=executor.job_name)
+        job.status = status
+        job.job_number = 42340793 + 2 * ii
+
+        executor.job_list.append(job)
+
+    with patch("subprocess.run", return_value=qstat_xml("tests/job/qstat_dat 2r_1qw.txt")):
+        slots = executor.available_slots()
+        assert slots == 1
+
+
+def test_available_slots_full_running(executor: AbciJobExecutor) -> None:
+    """
+    - Job status:
+        - RUNNING: 4
+        - WAITING: 0
+    - available: 0
+    """
+
+    for ii in range(4):
+        job = AbciJob(executor.job_filename, executor.job_group, job_name=executor.job_name)
+        job.status = JobStatus.RUNNING
+        job.job_number = 42340793 + 2 * ii
+
+        executor.job_list.append(job)
+
+    with patch("subprocess.run", return_value=qstat_xml("tests/job/qstat_dat.txt")):
+        result = executor.available_slots()
+        assert result == 0
+
+
+def test_available_slots_empty(executor: AbciJobExecutor) -> None:
+    """
+    - Job status:
+        - RUNNING: 0
+        - WAITING: 0
+    - available: 4
+    """
+    with patch("subprocess.run", return_value=qstat_xml("tests/job/qstat_dat_empty.txt")):
+        result = executor.available_slots()
+        assert result == 4
+
+
+def test_collect_finished(executor: AbciJobExecutor) -> None:
+    job_list: list[AbciJob] = []
+    for status in [JobStatus.FINISHED, JobStatus.RUNNING, JobStatus.FINISHED]:
+        job = MagicMock(spec=AbciJob)
+        job.status = status
+
+        job_list.append(job)
+        executor.job_list.append(job)
+
+    result = executor.collect_finished()
+    assert result == [job_list[0], job_list[2]]
+    assert executor.job_list == [job_list[1]]
+
+
+def test_collect_finished_empty(executor: AbciJobExecutor) -> None:
+    executor.job_list = []
+
+    result = executor.collect_finished()
+    assert result == []
+    assert executor.job_list == []
