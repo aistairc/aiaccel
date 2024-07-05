e3ec6a1dd8c4366c4b9a8572ef152a5d63ac5d64
aramoto99 <aramoto.masahiro@gmail.com>
2024-07-05 16:44:55 +0900

    Refactor Suggest class to use generics for improved type safety

--- aiaccel/apps/optimize.py
+++ aiaccel/apps/optimize.py
@@ -1,13 +1,17 @@
 import argparse
 import pickle as pkl
+from collections.abc import Callable
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 from hydra.utils import instantiate
 from omegaconf import OmegaConf as oc  # noqa: N813
+from optuna.trial import Trial
 
 from aiaccel.job import AbciJobExecutor
+
+T = TypeVar('T')
 
 """
 Usage (if parameters are not defined in a config file):
@@ -46,78 +50,79 @@ group: gaa50000
 
 """
 
-
 @dataclass
-class Suggest:
-    def __call__(self, trial: Any) -> Any:
+class Suggest(Generic[T]):
+    name: str
+
+    def __call__(self, trial: Any) -> T:
         raise NotImplementedError
 
 
 @dataclass
-class Const(Suggest):
-    name: str
-    value: Any
+class Const(Suggest[T]):
+    value: T
 
-    def __call__(self, _: Any) -> Any:
+    def __call__(self, _: Any) -> T:
         return self.value
 
 
 @dataclass
-class SuggestFloat(Suggest):
+class SuggestFloat(Generic[T]):
     name: str
     low: float
     high: float
-    step: float | None = None
-    log: bool | None = False
+    step: float | None | None = None
+    log: bool = False
 
-    def __call__(self, trial: Any) -> float:
-        return trial.suggest_float(name=self.name, low=self.low, high=self.high, step=self.step, log=self.log)  # type: ignore
+    def __call__(self, trial: Trial) -> float:
+        return trial.suggest_float(self.name, self.low, self.high, step=self.step, log=self.log)
 
 
 @dataclass
-class SuggestInt(Suggest):
+class SuggestInt(Generic[T]):
     name: str
     low: int
     high: int
-    step: int | None = 1
-    log: bool | None = False
+    step: int = 1
+    log: bool = False
 
-    def __call__(self, trial: Any) -> int:
-        return trial.suggest_int(name=self.name, low=self.low, high=self.high, step=self.step, log=self.log)  # type: ignore
+    def __call__(self, trial: Trial) -> int:
+        return trial.suggest_int(name=self.name, low=self.low, high=self.high, step=self.step, log=self.log)
 
 
 @dataclass
-class SuggestXXX(Suggest): ...
+class SuggestXXX: ...
 
 
 ...
 
 
 class HparamsManager:
-    def __init__(self, **params_def: dict[str, int | float | list[float | int] | Suggest]) -> None:
-        self.params = {}
+    def __init__(self, **params_def: dict[str, int | float | str | list[int | float] | Suggest[T]]) -> None:
+        self.params: dict[str, Callable[[Trial], Any]] = {}
         for name, param in params_def.items():
-            if not callable(param):
+            if callable(param):
+                self.params[name] = param
+            else:
                 if isinstance(param, list):
                     low, high = param
-                    param = lambda trial, name=name, low=low, high=high: SuggestFloat(name, low, high)(trial)  # type: ignore # noqa: E731
+                    self.params[name] = SuggestFloat(name=name, low=low, high=high)
                 else:
-                    param = lambda trial, name=name, value=param: Const(name, value)(trial)  # type: ignore # noqa: E731
-            self.params[name] = param
+                    self.params[name] = Const(name=name, value=param)
 
-    def suggest_hparams(self, trial: Any) -> dict[str, int | float | str]:
-        return {name: param_fn(trial) for name, param_fn in self.params.items()}  # type: ignore
+    def suggest_hparams(self, trial: Trial) -> dict[str, float | int | str | list[float | int | str]]:
+        return {name: param_fn(trial) for name, param_fn in self.params.items()}
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("job_filename", type=str, help="The shell script to execute.")
+    parser.add_argument("job_filename", type=Path, help="The shell script to execute.")
     parser.add_argument("--config", nargs="?", default=None)
 
     args, unk_args = parser.parse_known_args()
     config = oc.merge(oc.load(args.config), oc.from_cli(unk_args))
 
-    jobs = AbciJobExecutor(Path(args.job_filename), config.group, n_max_jobs=config.n_max_jobs)
+    jobs = AbciJobExecutor(args.job_filename, config.group, n_max_jobs=config.n_max_jobs)
     study = instantiate(config.study)
     params = instantiate(config.params)
 
@@ -125,13 +130,13 @@ def main() -> None:
 
     finished_job_count = 0
 
-    while finished_job_count <= config.n_trials:
+    while finished_job_count < config.n_trials:
         n_max_jobs = min(jobs.available_slots(), config.n_trials - finished_job_count)
         for _ in range(n_max_jobs):
             trial = study.ask()
 
             hparams = params.suggest_hparams(trial)
-            print(hparams)
+
             jobs.job_name = str(jobs.job_filename) + f"_{trial.number}"
 
             job = jobs.submit(
