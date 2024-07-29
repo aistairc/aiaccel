505502c72b289c897951397a15ef7a3a08cf8172
KanaiYuma <kanai-itc1013@aist.go.jp>
2024-07-29 13:50:20 +0900

    Remove source.

--- examples/hpo/samplers/coco/nelder-mead/objective.sh
+++ examples/hpo/samplers/coco/nelder-mead/objective.sh
@@ -2,10 +2,10 @@
 
 #$-l rt_C.small=1
 #$-cwd
-#$ -l h_rt=1:20:00
+#$ -l h_rt=1:00:00
 
 source /etc/profile.d/modules.sh
 module load gcc/13.2.0
 module load python/3.10/3.10.14 
 
-python3.10 experiment_for_nm_sampler_subTPE_parallel.py $@
+python3.10 experiment_for_nm_sampler_parallel.py $@
