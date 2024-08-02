15eed05e1a9724f0360f999926036312372258b8
KanaiYuma <kanai-itc1013@aist.go.jp>
2024-08-02 14:42:13 +0900

    Add execute_times, instances and optuna_seeds.

--- examples/hpo/samplers/coco/TPE/main_parallel_TPE.py
+++ examples/hpo/samplers/coco/TPE/main_parallel_TPE.py
@@ -4,21 +4,34 @@ from aiaccel.job import AbciJobExecutor
 
 
 def main() -> None:
+    job_filename = Path("objective.sh")
+    job_group = "xxx"
+
+    jobs = AbciJobExecutor(job_filename, job_group)
+
+    func_ids = list(range(1, 25))
     dims = [2, 3, 5, 10, 20, 40]
+    execute_times = ["0:01:00", "0:02:00", "0:03:00", "0:10:00", "0:30:00", "3:00:00"]
+    instances = list(range(1, 16))
+    optuna_seeds = list(range(1, 16))
 
-    for dim in dims:
-        job_filename = Path(f"objective_dim{dim}.sh")
-        job_group = "gaa50073"
-
-        jobs = AbciJobExecutor(job_filename, job_group)
-
-        func_ids = list(range(1, 25))
-
-        for func_id in func_ids:
-            jobs.submit(
-                args=["--func_id", f"{func_id}", "--dim", f"{dim}"],
-            )
-            print(f"submit --func_id {func_id} --dim {dim}")
+    for func_id in func_ids:
+        for dim, execute_time in zip(dims, execute_times, strict=False):
+            for instance, optuna_seed in zip(instances, optuna_seeds, strict=False):
+                jobs.submit(
+                    args=[
+                        "-l",
+                        f"h_rt={execute_time}",
+                        "--func_id",
+                        f"{func_id}",
+                        "--dim",
+                        f"{dim}",
+                        "--instance",
+                        f"{instance}",
+                        "--optuna_seed",
+                        f"{optuna_seed}",
+                    ],
+                )
 
 
 if __name__ == "__main__":
--- examples/hpo/samplers/coco/nelder-mead/main_parallel_nelder-mead.py
+++ examples/hpo/samplers/coco/nelder-mead/main_parallel_nelder-mead.py
@@ -5,18 +5,32 @@ from aiaccel.job import AbciJobExecutor
 
 def main() -> None:
     job_filename = Path("objective.sh")
-    job_group = "gaa50073"
+    job_group = "xxx"
 
     jobs = AbciJobExecutor(job_filename, job_group)
 
     func_ids = list(range(1, 25))
     dims = [2, 3, 5, 10, 20, 40]
+    instances = list(range(1, 16))
+    optuna_seeds = list(range(1, 16))
 
     for func_id in func_ids:
         for dim in dims:
-            jobs.submit(
-                args=["--func_id", f"{func_id}", "--dim", f"{dim}"],
-            )
+            for instance, optuna_seed in zip(instances, optuna_seeds, strict=False):
+                jobs.submit(
+                    args=[
+                        "-l",
+                        "h_rt=0:05:00",
+                        "--func_id",
+                        f"{func_id}",
+                        "--dim",
+                        f"{dim}",
+                        "--instance",
+                        f"{instance}",
+                        "--optuna_seed",
+                        f"{optuna_seed}",
+                    ],
+                )
 
 
 if __name__ == "__main__":
