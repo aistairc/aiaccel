5ae2391ed9b86dd2c67d547f2b850b2a4b8021e8
KanaiYuma <kanai-itc1013@aist.go.jp>
2024-08-06 14:56:05 +0900

    Use AbciJob in objective.sh.

--- examples/hpo/samplers/coco/TPE/main_parallel_TPE.py
+++ examples/hpo/samplers/coco/TPE/main_parallel_TPE.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 
-from aiaccel.job import AbciJobExecutor
+from aiaccel.job.abci_job import AbciJob
 
 
 def main() -> None:
     job_filename = Path("objective.sh")
     job_group = "xxx"
-
-    jobs = AbciJobExecutor(job_filename, job_group)
 
     func_ids = list(range(1, 25))
     dims = [2, 3, 5, 10, 20, 40]
@@ -18,10 +16,14 @@ def main() -> None:
     for func_id in func_ids:
         for dim, execute_time in zip(dims, execute_times, strict=False):
             for instance, optuna_seed in zip(instances, optuna_seeds, strict=False):
-                jobs.submit(
-                    args=[
+                job = AbciJob(
+                    job_filename,
+                    job_group,
+                    qsub_args=[
                         "-l",
                         f"h_rt={execute_time}",
+                    ],
+                    args=[
                         "--func_id",
                         f"{func_id}",
                         "--dim",
@@ -32,6 +34,7 @@ def main() -> None:
                         f"{optuna_seed}",
                     ],
                 )
+                job.submit()
 
 
 if __name__ == "__main__":
--- examples/hpo/samplers/coco/nelder-mead/main_parallel_nelder-mead.py
+++ examples/hpo/samplers/coco/nelder-mead/main_parallel_nelder-mead.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 
-from aiaccel.job import AbciJobExecutor
+from aiaccel.job.abci_job import AbciJob
 
 
 def main() -> None:
     job_filename = Path("objective.sh")
     job_group = "xxx"
-
-    jobs = AbciJobExecutor(job_filename, job_group)
 
     func_ids = list(range(1, 25))
     dims = [2, 3, 5, 10, 20, 40]
@@ -17,10 +15,14 @@ def main() -> None:
     for func_id in func_ids:
         for dim in dims:
             for instance, optuna_seed in zip(instances, optuna_seeds, strict=False):
-                jobs.submit(
-                    args=[
+                job = AbciJob(
+                    job_filename,
+                    job_group,
+                    qsub_args=[
                         "-l",
                         "h_rt=0:05:00",
+                    ],
+                    args=[
                         "--func_id",
                         f"{func_id}",
                         "--dim",
@@ -31,6 +33,7 @@ def main() -> None:
                         f"{optuna_seed}",
                     ],
                 )
+                job.submit()
 
 
 if __name__ == "__main__":
