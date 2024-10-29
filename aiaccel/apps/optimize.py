3f7553806c444367d4935c8ef3bf6fc07db1437f
aramoto99 <aramoto.masahiro@gmail.com>
2024-10-29 15:20:49 +0900

    Remove unnecessary code

--- aiaccel/apps/optimize.py
+++ aiaccel/apps/optimize.py
@@ -9,6 +9,7 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf as oc  # noqa: N813
 
 from optuna.trial import Trial
+from optuna.samplers import PartialFixedSampler
 
 from aiaccel.hpo.optuna.suggest_wrapper import Const, Suggest, SuggestFloat, T
 from aiaccel.job import AbciJobExecutor, BaseJobExecutor, LocalJobExecutor
@@ -48,6 +49,11 @@ n_max_jobs: 4
 
 group: gaa50000
 
+additional_scenario:  # Optional
+  - n_trials: 30
+    n_max_jobs: 4
+    fixed_params:
+      params: ["x1"]
 """
 
 
@@ -67,17 +73,46 @@ class HparamsManager:
     def suggest_hparams(self, trial: Trial) -> dict[str, float | int | str | list[float | int | str]]:
         return {name: param_fn(trial) for name, param_fn in self.params.items()}
 
+    def update_fixed_params(self, sampler) -> None:
+        """Update parameters based on the sampler's fixed parameters"""
+        if hasattr(sampler, "fixed_params"):
+            for name, value in sampler.fixed_params.items():
+                self.params[name] = Const(name=name, value=value)
+
+
+def run_trials(
+    study, params: HparamsManager, jobs: BaseJobExecutor, n_trials: int, result_filename_template: str
+) -> None:
+    """Run trials and collect results"""
+    finished_job_count = 0
+    while finished_job_count < n_trials:
+        n_running_jobs = len(jobs.get_running_jobs())
+        n_max_jobs = min(jobs.available_slots(), n_trials - finished_job_count - n_running_jobs)
+        for _ in range(n_max_jobs):
+            trial = study.ask()
+            hparams = params.suggest_hparams(trial)
+            jobs.job_name = str(jobs.job_filename) + f"_{trial.number}"
+            job = jobs.submit(
+                args=[result_filename_template] + sum([[f"--{k}", f"{v}"] for k, v in hparams.items()], []),
+                tag=trial,
+            )
+        for job in jobs.collect_finished():
+            trial = job.tag
+            with open(result_filename_template.format(job=job), "rb") as f:
+                y = pkl.load(f)
+            study.tell(trial, y)
+            print(f"Trial {trial.number} finished with value {y}, params: {trial.params}")
+            finished_job_count += 1
+
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("job_filename", type=Path, help="The shell script to execute.")
     parser.add_argument("--config", nargs="?", default=None)
     parser.add_argument("--executor", nargs="?", default="local")
-    parser.add_argument("--load_if_exists", action="store_true", help="Load the study if it exists.", default=False)
 
     args, unk_args = parser.parse_known_args()
     config = oc.merge(oc.load(args.config), oc.from_cli(unk_args))
-    config.study.load_if_exists = args.load_if_exists
 
     jobs: BaseJobExecutor
 
@@ -93,32 +128,24 @@ def main() -> None:
 
     result_filename_template = "{job.cwd}/{job.job_name}_result.pkl"
 
-    finished_job_count = 0
+    run_trials(study, params, jobs, config.n_trials, result_filename_template)
 
-    while finished_job_count < config.n_trials:
-        n_running_jobs = len(jobs.get_running_jobs())
-        n_max_jobs = min(jobs.available_slots(), config.n_trials - finished_job_count - n_running_jobs)
-        for _ in range(n_max_jobs):
-            trial = study.ask()
+    if "additional_scenario" in config:
+        print("Additional scenarios detected. Running additional scenarios.")
+        for scenario in config.additional_scenario:
+            fixed_params = {}
+            if "params" in scenario.get("fixed_params", {}):
+                for param in scenario["fixed_params"]["params"]:
+                    fixed_params[param] = study.best_params[param]
+            if "values" in scenario.get("fixed_params", {}):
+                fixed_params.update(scenario["fixed_params"]["values"])
+            if fixed_params:
+                base_sampler = instantiate(config.study.sampler)
+                study.sampler = PartialFixedSampler(fixed_params=fixed_params, base_sampler=base_sampler)
+                params.update_fixed_params(study.sampler)
+            run_trials(study, params, jobs, scenario["n_trials"], result_filename_template)
 
-            hparams = params.suggest_hparams(trial)
-
-            jobs.job_name = str(jobs.job_filename) + f"_{trial.number}"
-
-            job = jobs.submit(
-                args=[result_filename_template] + sum([[f"--{k}", f"{v:.5f}"] for k, v in hparams.items()], []),
-                tag=trial,
-            )
-
-        for job in jobs.collect_finished():
-            trial = job.tag
-
-            with open(result_filename_template.format(job=job), "rb") as f:
-                y = pkl.load(f)
-
-            study.tell(trial, y)
-
-            finished_job_count += 1
+    print("Best params:", study.best_params)
 
 
 if __name__ == "__main__":
