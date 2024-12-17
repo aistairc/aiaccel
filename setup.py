2d0ddc4c32e014aaf0c4424b6bc52c5b2604f0d3
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2024-12-17 17:45:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-hpo = [
+hpo_dependencies = [
     "numpy",
     "optuna>=3.4.0",
 ]
-torch = ["hydra-core", "lightning>=2.2.1", "torch>=2.2.0", "h5py"]
-dev = [
+torch_dependencies = ["hydra-core", "lightning>=2.2.1", "torch>=2.2.0", "h5py"]
+dev_dependencies = [
     "mypy",
     "myst-parser",
     "pre-commit",
@@ -27,19 +27,19 @@ dev = [
     "pandas-stubs",
     "matplotlib",
 ]
-github_actions = [
+github_actions_dependencies = [
     "pytest-github-actions-annotate-failures",
 ]
-all_dependencies = hpo + torch
-all_dev_dependencies = hpo + torch + dev + github_actions
+all_dependencies = hpo_dependencies + torch_dependencies
+all_dev_dependencies = hpo_dependencies + torch_dependencies + dev_dependencies + github_actions_dependencies
 
 setup(
     extras_require={
         "all": all_dependencies,
         "all-dev": all_dev_dependencies,
-        "hpo": hpo,
-        "torch": torch,
-        "dev": dev,
-        "github-actions": github_actions,
+        "hpo": hpo_dependencies,
+        "torch": torch_dependencies,
+        "dev": dev_dependencies,
+        "github-actions": github_actions_dependencies,
     },
 )
