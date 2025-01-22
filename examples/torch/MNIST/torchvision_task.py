10670be1238cfa0b1db8f9ffa677e3c1fea59a42
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-01-22 17:01:06 +0900

    Fix for lint.

--- examples/torch/MNIST/torchvision_task.py
+++ examples/torch/MNIST/torchvision_task.py
@@ -4,8 +4,9 @@ from torch import Tensor, nn
 from torch.nn import functional as func
 from torch.utils.data import DataLoader
 
-from aiaccel.torch.lightning import OptimizerConfig, OptimizerLightningModule
 import torchmetrics
+
+from aiaccel.torch.lightning import OptimizerConfig, OptimizerLightningModule
 
 
 class Resnet50Task(OptimizerLightningModule):
@@ -14,9 +15,9 @@ class Resnet50Task(OptimizerLightningModule):
         self.model = model
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
-        self.train_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=10)
-        self.val_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=10)
-        self.test_accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=10)
+        self.train_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=10)
+        self.val_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=10)
+        self.test_accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=10)
 
     def forward(self, x: Any) -> Any:
         return self.model(x)
@@ -27,8 +28,8 @@ class Resnet50Task(OptimizerLightningModule):
         loss = func.cross_entropy(logits, y)
 
         acc = self.train_accuracy(logits, y)
-        self.log('train_loss', loss, prog_bar=True)
-        self.log('train_acc', acc, prog_bar=True)
+        self.log("train_loss", loss, prog_bar=True)
+        self.log("train_acc", acc, prog_bar=True)
         return loss
 
     def validation_step(self, batch: DataLoader[Any], batch_idx: int) -> None:
@@ -37,5 +38,5 @@ class Resnet50Task(OptimizerLightningModule):
         loss = func.cross_entropy(logits, y)
 
         acc = self.val_accuracy(logits, y)
-        self.log('val_loss', loss, prog_bar=True)
-        self.log('val_acc', acc, prog_bar=True)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
