dd0f2ade77daf1ba84c69deedd89702023f4c051
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-01-28 13:32:02 +0900

    Add ddp config and script.

---
+++ examples/torch/MNIST/train_ddp.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#PBS -q rt_HF
+#PBS -l select=1
+#PBS -l walltime=1:00:00
+#PBS -P grpname
+#PBS -j oe
+
+cd ${PBS_O_WORKDIR}
+
+source /etc/profile.d/modules.sh
+module load cuda/12.6/12.6.1
+
+source path_to_aiaccel_env/bin/activate
+
+wd=path_to_working_directory
+
+singularity exec --nv path_to_python.sif python -m aiaccel.torch.apps.train $wd/config_ddp.yaml --working_directory $wd
