251e1f1801300ed215342d7348c98874ae6431cf
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-02-06 15:46:07 +0900

    Remove aiaccel_env.

--- examples/torch/MNIST/train.sh
+++ examples/torch/MNIST/train.sh
@@ -11,8 +11,6 @@ cd ${PBS_O_WORKDIR}
 source /etc/profile.d/modules.sh
 module load cuda/12.6/12.6.1
 
-source path_to_aiaccel_env/bin/activate
-
 wd=path_to_working_directory
 
 singularity exec --nv path_to_python.sif python -m aiaccel.torch.apps.train $wd/config.yaml --working_directory $wd
