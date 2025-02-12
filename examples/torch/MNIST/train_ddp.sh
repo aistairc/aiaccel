fa2293517823b3cc42c63115950b5012f6c8bf8a
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-02-12 14:55:17 +0900

    Fix for DDP.

--- examples/torch/MNIST/train_ddp.sh
+++ examples/torch/MNIST/train_ddp.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #PBS -q rt_HF
-#PBS -l select=1
+#PBS -l select=1:mpiprocs=192
 #PBS -l walltime=1:00:00
 #PBS -P grpname
 #PBS -j oe
@@ -9,8 +9,12 @@
 cd ${PBS_O_WORKDIR}
 
 source /etc/profile.d/modules.sh
-module load cuda/12.6/12.6.1
+module load hpcx
 
 wd=path_to_working_directory
+num_gpus=$(nvidia-smi -L | wc -l)
 
-singularity exec --nv aiaccel.sif python -m aiaccel.torch.apps.train $wd/config_ddp.yaml --working_directory $wd
+mpirun -np $num_gpus -bind-to none -map-by slot \
+        -x MAIN_ADDR=$(hostname -i)  \
+        -x MAIN_PORT=3000 \
+        singularity exec --nv aiaccel.sif python -m aiaccel.torch.apps.train $wd/config_ddp.yaml --working_directory $wd
