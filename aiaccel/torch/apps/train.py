2ce430fda9cbde41741a45d50f28090a7b2cff24
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-03-08 15:54:41 +0900

    Update train.py (#430)
    * Update train.py
    
    * Remove docstrfmt from precommit temporally
    
    * Revert doc-string of train.py

--- aiaccel/torch/apps/train.py
+++ aiaccel/torch/apps/train.py
@@ -1,13 +1,21 @@
 from argparse import ArgumentParser
+import logging
 import os
 from pathlib import Path
 
 from hydra.utils import instantiate
 from omegaconf import OmegaConf as oc  # noqa: N813
 
-import lightning as lt
+from aiaccel.utils import (
+    collect_git_status_from_config,
+    load_config,
+    overwrite_omegaconf_dumper,
+    pathlib2str_config,
+    print_config,
+    print_git_status,
+)
 
-from aiaccel.utils import load_config, pathlib2str_config, print_config
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
@@ -35,36 +43,77 @@ def main() -> None:
         ```
     """
 
+    overwrite_omegaconf_dumper()
+
     parser = ArgumentParser()
     parser.add_argument("config", type=str, help="Config file in YAML format")
-    parser.add_argument("--working_directory", type=str, default=str(Path.cwd()), help="Working directory")
+    parser.add_argument(
+        "-s",
+        "--submit_job",
+        action="store_true",
+        help="Submit a training job using the command specified in the config file",
+    )
+    parser.add_argument(
+        "-p",
+        "--print_script",
+        action="store_true",
+        help="Generate the training command from the config file and print it",
+    )
     args, unk_args = parser.parse_known_args()
 
     # load config
-    config = load_config(args.config, {"base_config_path": str(Path(__file__).parent / "config")} | vars(args))
-    config = oc.merge(config, oc.from_cli(unk_args))
-
-    if int(os.environ.get("OMPI_COMM_WORLD_RANK", 0)) == 0 and int(os.environ.get("RANK", 0)) == 0:
-        print_config(config)
-
-    # build trainer
-    trainer: lt.Trainer = instantiate(config.trainer)
-
-    # save config
-    if trainer.is_global_zero:
-        if "merged_config_path" in config:
-            merged_config_path = config.merged_config_path
-        else:
-            merged_config_path = Path(config.working_directory) / "config_merged.yaml"
-
-        with open(merged_config_path, "w") as f:
-            oc.save(pathlib2str_config(config), f)
-
-    # start training
-    trainer.fit(
-        model=instantiate(config.task),
-        datamodule=instantiate(config.datamodule),
+    config = oc.merge(
+        load_config(
+            args.config,
+            {
+                "config_filename": args.config,
+                "working_directory": str(Path(args.config).parent.resolve()),
+                "base_config_path": str(Path(__file__).parent / "config"),
+            },
+        ),
+        oc.from_cli(unk_args),
     )
+
+    if args.print_script:
+        print(config.train_script)
+    elif args.submit_job:
+        import subprocess
+        from tempfile import NamedTemporaryFile
+
+        status_list = collect_git_status_from_config(config)
+        print_git_status(status_list)
+
+        if not all(st.ready() for st in status_list):
+            logging.error("There are remaining uncommited file(s).")
+        else:
+            with NamedTemporaryFile("w", suffix=".sh") as f:
+                f.write(config.train_script)
+
+                subprocess.run(config.job_submission_script.format(job_filename=f.name), shell=True)
+    else:
+        import lightning as lt
+
+        if int(os.environ.get("OMPI_COMM_WORLD_RANK", 0)) == 0 and int(os.environ.get("RANK", 0)) == 0:
+            print_config(config)
+
+        # build trainer
+        trainer: lt.Trainer = instantiate(config.trainer)
+
+        # save config
+        if trainer.is_global_zero:
+            if "merged_config_path" in config:
+                merged_config_path = config.merged_config_path
+            else:
+                merged_config_path = Path(config.working_directory) / "config_merged.yaml"
+
+            with open(merged_config_path, "w") as f:
+                oc.save(pathlib2str_config(config), f)
+
+        # start training
+        trainer.fit(
+            model=instantiate(config.task),
+            datamodule=instantiate(config.datamodule),
+        )
 
 
 if __name__ == "__main__":
