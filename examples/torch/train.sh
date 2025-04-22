40f59ece6fb07a12d72c97e3b4d650d2a0e5edb6
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-04-22 16:18:06 +0900

    Remove Singrality.

--- examples/torch/train.sh
+++ examples/torch/train.sh
@@ -5,12 +5,15 @@
 #PBS -l walltime=1:00:00
 #PBS -P grpname
 #PBS -j oe
+#PBS -k oed
 
 cd ${PBS_O_WORKDIR}
 
 source /etc/profile.d/modules.sh
 module load cuda/12.6/12.6.1
+module load python/3.13/3.13.2
+source path_to_venv/bin/activate
 
 wd=path_to_working_directory
 
-singularity exec --nv aiaccel.sif python -m aiaccel.torch.apps.train $wd/config.yaml --working_directory $wd
+python -m aiaccel.torch.apps.train $wd/config.yaml --working_directory $wd
