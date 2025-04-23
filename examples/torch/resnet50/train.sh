ce6a13a7de49b8ad74d2dcb35b95539a99851b99
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-04-23 15:24:01 +0900

    Update torch example

--- examples/torch/resnet50/train.sh
+++ examples/torch/resnet50/train.sh
@@ -3,17 +3,14 @@
 #PBS -q rt_HG
 #PBS -l select=1
 #PBS -l walltime=1:00:00
-#PBS -P grpname
 #PBS -j oe
 #PBS -k oed
 
 cd ${PBS_O_WORKDIR}
 
 source /etc/profile.d/modules.sh
+
 module load cuda/12.6/12.6.1
 module load python/3.13/3.13.2
-source path_to_venv/bin/activate
 
-wd=path_to_working_directory
-
-python -m aiaccel.torch.apps.train $wd/config.yaml --working_directory $wd
+python -m aiaccel.torch.apps.train resnet50/config.yaml
