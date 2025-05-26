44ec9841fda57d44071737a6a8d37e8f4ca7c5f9
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-05-26 16:05:33 +0900

    Fix version abci3.py.

--- aiaccel/jobs/cli/abci3.py
+++ aiaccel/jobs/cli/abci3.py
@@ -7,31 +7,11 @@ import shlex
 import subprocess
 import time
 
-job_script_template = """\
-#! /bin/bash
 
-#PBS -j oe
-#PBS -k oed
+def prepare_mpi_job(args: Namespace, command: str) -> tuple[str, list[str], list[Path]]:
+    lock_filename = args.log_filename.with_suffix(".lock")
 
-set -e
-
-if [ -n "$PBS_O_WORKDIR" ] && [ "$PBS_ENVIRONMENT" != "PBS_INTERACTIVE" ]; then
-    cd $PBS_O_WORKDIR
-fi
-
-echo Job ID: $PBS_JOBID
-echo Hostname: $(hostname)
-
-export NVIDIA_VISIBLE_DEVICES=all
-export SINGULARITY_BINDPATH="$SINGULARITY_BINDPATH,/groups,/groups-2.0"
-
-export SINGULARITYENV_COLUMNS=120
-export SINGULARITYENV_PYTHONUNBUFFERED=true
-
-{job}
-"""
-
-mpi_job_template = """\
+    job = f"""\
 source /etc/profile.d/modules.sh
 module load hpcx
 
@@ -43,45 +23,6 @@ mpirun -bind-to none -map-by slot \\
 
 rm {lock_filename}\
 """
-
-array_job_template = """\
-for LOCAL_PROC_INDEX in {{1..{n_procs_per_job}}}; do
-    TASK_INDEX=$(( PBS_ARRAY_INDEX + {n_tasks_per_proc} * (LOCAL_PROC_INDEX - 1) ))
-
-    if [[ $TASK_INDEX -ge {n_tasks} ]]; then
-        break
-    fi
-
-    CUDA_VISIBLE_DEVICES=$(( LOCAL_PROC_INDEX % 8 )) \\
-    TASK_INDEX=$TASK_INDEX \\
-    TASK_STEPSIZE={n_tasks_per_proc} \\
-        {command} > {args.log_filename.with_suffix(".${{PBS_ARRAY_INDEX}}.proc-${{LOCAL_PROC_INDEX}}.log")} 2>&1 \\
-        && rm {args.log_filename.with_suffix(".${{PBS_ARRAY_INDEX}}.proc-${{LOCAL_PROC_INDEX}}.lock")} &
-done
-wait\
-"""
-
-train_job_template = """\
-source /etc/profile.d/modules.sh
-module load hpcx
-
-mpirun -bind-to none -map-by slot \\
-    -mca pml ob1 -mca btl self,tcp -mca btl_tcp_if_include bond0 \\
-    -x MAIN_ADDR=$(hostname -i) \\
-    -x MAIN_PORT=3000 \\
-    -x COLUMNS=120 \\
-    -x PYTHONUNBUFFERED=true \\
-    singularity exec --nv $singularity_path direnv exec . \\
-    {command}
-
-rm {lock_filename}\
-"""
-
-
-def prepare_mpi_job(args: Namespace, command: str) -> tuple[str, list[str], list[Path]]:
-    lock_filename = args.log_filename.with_suffix(".lock")
-
-    job = mpi_job_template.format(command=command, lock_filename=lock_filename)
 
     n_mpiprocs = args.n_procs // args.n_nodes
     n_ompthreads = (args.n_nodes * 96) // args.n_procs
@@ -103,14 +44,26 @@ def prepare_single_job(args: Namespace, command: str) -> tuple[str, list[str], l
 
 
 def prepare_array_jobs(args: Namespace, command: str) -> tuple[str, list[str], list[Path]]:
+    assert args.n_tasks_per_proc % args.n_procs_per_job == 0
+
     n_tasks_per_proc = args.n_tasks_per_proc // args.n_procs_per_job
 
-    job = array_job_template.format(
-        command=command,
-        n_procs_per_job=args.n_procs_per_job,
-        n_tasks_per_proc=n_tasks_per_proc,
-        n_tasks=args.n_tasks,
-    )
+    job = f"""\
+for LOCAL_PROC_INDEX in {{1..{args.n_procs_per_job}}}; do
+    TASK_INDEX=$(( PBS_ARRAY_INDEX + {n_tasks_per_proc} * (LOCAL_PROC_INDEX - 1) ))
+
+    if [[ $TASK_INDEX -gt {args.n_tasks} ]]; then
+        break
+    fi
+
+    CUDA_VISIBLE_DEVICES=$(( LOCAL_PROC_INDEX % 8 )) \\
+    TASK_INDEX=$TASK_INDEX \\
+    TASK_STEPSIZE={n_tasks_per_proc} \\
+        {command} > {args.log_filename.with_suffix(".${PBS_ARRAY_INDEX}.proc-${LOCAL_PROC_INDEX}.log")} 2>&1 \\
+        && rm {args.log_filename.with_suffix(".${PBS_ARRAY_INDEX}.proc-${LOCAL_PROC_INDEX}.lock")} &
+done
+wait\
+"""
 
     qsub_args = ["-l", "select=1"]
     qsub_args += ["-J", f"1-{args.n_tasks}:{args.n_tasks_per_proc}"]
@@ -123,7 +76,7 @@ def prepare_array_jobs(args: Namespace, command: str) -> tuple[str, list[str], l
             if task_index >= args.n_tasks:
                 break
 
-            lock_filename_list.append(args.log_filename.with_suffix(f".{array_idx + 1}.proc-{local_proc_idx + 1}.lock"))
+            lock_filename_list.append(args.log_filename.with_suffix(f".{array_idx+1}.proc-{local_proc_idx+1}.lock"))
 
     return job, qsub_args, lock_filename_list
 
@@ -131,10 +84,22 @@ def prepare_array_jobs(args: Namespace, command: str) -> tuple[str, list[str], l
 def prepare_train_job(args: Namespace, command: str) -> tuple[str, list[str], list[Path]]:
     lock_filename = args.log_filename.with_suffix(".lock")
 
-    job = train_job_template.format(command=command, lock_filename=lock_filename)
+    job = f"""\
+source /etc/profile.d/modules.sh
+module load hpcx
+
+mpirun -bind-to none -map-by slot \\
+    -mca pml ob1 -mca btl self,tcp -mca btl_tcp_if_include bond0 \\
+    -x MAIN_ADDR=$(hostname -i) \\
+    -x MAIN_PORT=3000 \\
+    -x COLUMNS=120 \\
+    -x PYTHONUNBUFFERED=true \\
+    {command} && \\
+rm {lock_filename}\
+"""
 
     n_nodes = args.n_gpus // 8
-    qsub_args = ["-l", f"select={n_nodes}:n_mpiprocs=8:ompthreads=12"]
+    qsub_args = ["-l", f"select={n_nodes}:mpiprocs=8:ompthreads=12"]
     qsub_args += ["-o", str(args.log_filename)]
 
     return job, qsub_args, [lock_filename]
@@ -144,7 +109,7 @@ def main() -> None:
     parent_parser = ArgumentParser(add_help=False)
     parent_parser.add_argument("--local", action="store_true")
     parent_parser.add_argument("--use_singularity", action="store_true")
-    parent_parser.add_argument("--walltime", type=str, default="6:0:0")
+    parent_parser.add_argument("--walltime", type=str, default="0:40:0")
     parent_parser.add_argument("log_filename", type=Path)
     parent_parser.add_argument("command", nargs="+")
 
@@ -154,7 +119,7 @@ def main() -> None:
     sub_parser = sub_parsers.add_parser("cpu", parents=[parent_parser])
     sub_parser.add_argument("--n_tasks", type=int)
     sub_parser.add_argument("--n_tasks_per_proc", type=int, default=2048)
-    sub_parser.add_argument("--n_procs_per_job", type=int, default=48)
+    sub_parser.add_argument("--n_procs_per_job", type=int, default=32)
 
     sub_parser = sub_parsers.add_parser("gpu", parents=[parent_parser])
     sub_parser.add_argument("--n_tasks", type=int)
@@ -181,33 +146,55 @@ def main() -> None:
         venv_path = "./env"
         command = f"source {venv_path}/bin/activate && {shlex.join(args.command)}"
 
-    match args.mode:
-        case "cpu" | "gpu":
-            if args.n_tasks is None:
-                job, qsub_args, lock_filename_list = prepare_single_job(args, command)
-            else:
-                job, qsub_args, lock_filename_list = prepare_array_jobs(args, command)
-        case "mpi":
-            job, qsub_args, lock_filename_list = prepare_mpi_job(args, command)
-        case "train":
-            job, qsub_args, lock_filename_list = prepare_train_job(args, command)
-        case _:
-            raise ValueError()
+    if args.mode in ["cpu", "gpu"]:
+        if args.n_tasks is None:
+            job, qsub_args, lock_filename_list = prepare_single_job(args, command)
+        else:
+            job, qsub_args, lock_filename_list = prepare_array_jobs(args, command)
+    elif args.mode == "mpi":
+        job, qsub_args, lock_filename_list = prepare_mpi_job(args, command)
+    elif args.mode == "train":
+        job, qsub_args, lock_filename_list = prepare_train_job(args, command)
+    else:
+        raise ValueError()
 
     for lock_filename in lock_filename_list:
         lock_filename.touch()
 
     job_filename: Path = args.log_filename.with_suffix(".sh")
     with open(job_filename, "w") as f:
-        f.write(job_script_template.format(job=job))
+        f.write(
+            f"""\
+#! /bin/bash
+
+#PBS -j oe
+#PBS -k oed
+
+set -e
+
+if [ -n "$PBS_O_WORKDIR" ] && [ "$PBS_ENVIRONMENT" != "PBS_INTERACTIVE" ]; then
+    cd $PBS_O_WORKDIR
+fi
+
+echo Job ID: $PBS_JOBID
+echo Hostname: $(hostname)
+
+export NVIDIA_VISIBLE_DEVICES=all
+export SINGULARITY_BINDPATH="$SINGULARITY_BINDPATH,/groups,/groups-2.0"
+
+export SINGULARITYENV_COLUMNS=120
+export SINGULARITYENV_PYTHONUNBUFFERED=true
+
+{job}
+"""
+        )
 
     if not args.local:
-        qsub_args = ["qsub", "-P", os.environ["JOB_GROUP"], "-q", "rt_HF"]
-        qsub_args += ["-l", f"walltime={args.walltime}"]
-        qsub_args += qsub_args + [str(job_filename)]
+        qsub_command = ["qsub", "-P", os.environ["JOB_GROUP"], "-q", "rt_HF"]
+        qsub_command += ["-l", f"walltime={args.walltime}", "-v", "USE_SSH=1"]
+        qsub_command += qsub_args + [str(job_filename)]
 
-        qsub_command_str = shlex.join(qsub_args)
-        print(qsub_command_str)
+        qsub_command_str = shlex.join(qsub_command)
 
         subprocess.run(qsub_command_str, shell=True, check=True)
 
@@ -215,7 +202,7 @@ def main() -> None:
             while lock_filename.exists():
                 time.sleep(1.0)
     else:
-        subprocess.run(["bash", str(job_filename)])
+        subprocess.run(["bash", str(job_filename)], check=True)
 
 
 if __name__ == "__main__":
