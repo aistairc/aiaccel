750ebadef43e49a80fa802d248ea0fc72ac299cc
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-05-27 17:32:43 +0900

    Add hyphen.

--- aiaccel/jobs/cli/abci3.py
+++ aiaccel/jobs/cli/abci3.py
@@ -110,8 +110,8 @@ def main() -> None:  # noqa: C901
     parent_parser.add_argument("--local", action="store_true")
     parent_parser.add_argument("--use_singularity", action="store_true")
     parent_parser.add_argument("--walltime", type=str, default="0:40:0")
-    parent_parser.add_argument("log_filename", type=Path)
-    parent_parser.add_argument("command", nargs="+")
+    parent_parser.add_argument("--log_filename", type=Path)
+    parent_parser.add_argument("--command", nargs="+")
 
     parser = ArgumentParser()
     sub_parsers = parser.add_subparsers(dest="mode", required=True)
