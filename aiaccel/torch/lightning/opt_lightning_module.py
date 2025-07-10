50e3d6c7b984ab6308afd5d1501dbc612640fd32
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-07-10 15:45:35 +0900

    Extend OptimizerLightningModule

--- aiaccel/torch/lightning/opt_lightning_module.py
+++ aiaccel/torch/lightning/opt_lightning_module.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 from dataclasses import dataclass
+from fnmatch import fnmatch
+
+from torch import nn
 
 import lightning as lt
 from lightning.pytorch.utilities.types import OptimizerLRSchedulerConfig
@@ -15,13 +18,74 @@ if TYPE_CHECKING:
 @dataclass
 class OptimizerConfig:
     """
-    Configuration class for the optimizer and scheduler in the LightningModule.
+    Configuration for the optimizer and scheduler in a LightningModule.
+    Args:
+        optimizer_generator (Callable[..., optim.optimizer.Optimizer]): A callable that generates the optimizer.
+        params_transformer (Callable[..., Iterator[tuple[str, Any]]] | None): A callable that transforms the parameters
+            into a format suitable for the optimizer. If None, the parameters are used as is. Defaults to None.
+        scheduler_generator (Callable[..., optim.lr_scheduler.LRScheduler] | None):
+            A callable that generates the learning rate scheduler. If None, no scheduler is used. Defaults to None.
+        scheduler_interval (str | None): The interval at which the scheduler is called. Defaults to "step".
+        scheduler_monitor (str | None): The metric to monitor for the scheduler. Defaults to "validation/loss".
     """
 
     optimizer_generator: Callable[..., optim.optimizer.Optimizer]
+    params_transformer: Callable[..., Iterator[tuple[str, Any]]] | None = None
+
     scheduler_generator: Callable[..., optim.lr_scheduler.LRScheduler] | None = None
     scheduler_interval: str | None = "step"
     scheduler_monitor: str | None = "validation/loss"
+
+
+def build_param_groups(
+    named_params: Iterator[tuple[str, nn.Parameter]],
+    groups: list[dict[str, Any]],
+) -> list[dict[str, Any]]:
+    """
+    Build parameter groups for the optimizer based on the provided patterns.
+    Args:
+        named_params (Iterator[tuple[str, nn.Parameter]]): An iterator of named parameters.
+        groups (list[dict[str, Any]]): A list of dictionaries where each dictionary contains
+            a "pattern" key that specifies the parameter names to match (`fnmatch`), and other optional keys.
+
+    Returns:
+        list[dict[str, Any]]: A list of dictionaries representing parameter groups for the optimizer.
+
+    Example:
+    In your config file, you might have:
+    ```yaml
+    optimizer_config:
+        _target_: aiaccel.torch.lightning.OptimizerConfig
+      optimizer_generator:
+        _partial_: True
+        _target_: torch.optim.AdamW
+        weight_decay: 0.01
+      param_transformer:
+        _partial_: True
+        _target_: aiaccel.torch.lightning.build_param_groups
+        groups:
+            - pattern: "*bias"
+              lr: 0.01
+            - pattern: "*weight"
+              lr: 0.001
+    ```
+    This will create two parameter groups: one for biases with a learning rate of 0.01 and another for weights with
+    a learning rate of 0.001. The `build_param_groups` function will match the parameters based on the provided
+    patterns and return them in the appropriate format for the optimizer.
+    """
+    remaining = dict(named_params)
+
+    param_groups = []
+    for spec in groups:
+        matched_params = []
+        for target in [spec["pattern"]] if isinstance(spec["pattern"], str) else spec["pattern"]:
+            matched_params += [remaining.pop(name) for name in list(remaining.keys()) if fnmatch(name, target)]
+
+        param_groups.append({"params": matched_params} | {k: v for k, v in spec.items() if k != "pattern"})
+
+    param_groups.append({"params": list(remaining.values())})
+
+    return param_groups
 
 
 class OptimizerLightningModule(lt.LightningModule):
@@ -50,7 +114,13 @@ class OptimizerLightningModule(lt.LightningModule):
         Returns:
             Union[optim.optimizer.Optimizer, OptimizerLRSchedulerConfig]: The optimizer and scheduler configuration.
         """
-        optimizer = self.optcfg.optimizer_generator(params=self.parameters())
+
+        params = self.named_parameters()
+        if self.optcfg.params_transformer is not None:
+            params = self.optcfg.params_transformer(params)
+
+        optimizer = self.optcfg.optimizer_generator(params)
+
         if self.optcfg.scheduler_generator is None:
             return optimizer
         else:
