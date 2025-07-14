45a0809b1bd826cbb83adde74f3ae2f36b4f9bce
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-07-14 14:41:54 +0900

    Fix objective.py.

--- examples/hpo/objective.py
+++ examples/hpo/objective.py
@@ -1,3 +1,17 @@
+import argparse
+
+
 def main(x1: float, x2: float) -> float:
-    y = (x1**2) - (4.0 * x1) + (x2**2) - x2 - (x1 * x2)
+    y = (x1**2) + (x2**2)
     return y
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--x1", type=float)
+    parser.add_argument("--x2", type=float)
+
+    args, unk_args = parser.parse_known_args()
+
+    print(main(args.x1, args.x2))
