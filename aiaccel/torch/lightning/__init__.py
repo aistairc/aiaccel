3c6b6b517786234581dc176e7700369a2dcf7a87
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-07-19 00:45:23 +0900

    Extend OptimizerLightningModule (#474)
    * Extend OptimizerLightningModule
    
    * Update document
    
    * Update opt_lightning_module.py
    
    * Update opt_lightning_module.py
    
    * Updates

--- aiaccel/torch/lightning/__init__.py
+++ aiaccel/torch/lightning/__init__.py
@@ -1,4 +1,4 @@
 from aiaccel.torch.lightning.abci_environment import ABCIEnvironment
-from aiaccel.torch.lightning.opt_lightning_module import OptimizerConfig, OptimizerLightningModule
+from aiaccel.torch.lightning.opt_lightning_module import OptimizerConfig, OptimizerLightningModule, build_param_groups
 
-__all__ = ["ABCIEnvironment", "OptimizerConfig", "OptimizerLightningModule"]
+__all__ = ["ABCIEnvironment", "OptimizerConfig", "OptimizerLightningModule", "build_param_groups"]
