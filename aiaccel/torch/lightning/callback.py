5f7d84ab17f788aaa45d4d8b8cb1942b0fc5730a
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-07-23 10:02:25 +0900

    Replace param to metric.

--- aiaccel/torch/lightning/callback.py
+++ aiaccel/torch/lightning/callback.py
@@ -3,25 +3,25 @@ import json
 import lightning
 
 
-class SaveParamCallback(lightning.Callback):
+class SaveMetricCallback(lightning.Callback):
     """
-    Lightning Callback for save paramater in fit ends.
+    Lightning Callback for save metric in fit ends.
 
     Args:
-        param_name (str): Parameter name to save
+        metric_name (str): Metric name to save
         output_path (str): File name to save
     """
 
-    def __init__(self, param_name: str, output_path: str) -> None:
+    def __init__(self, metric_name: str, output_path: str) -> None:
         super().__init__()
-        self.param_name = param_name
+        self.metric_name = metric_name
         self.output_path = output_path
 
     def on_fit_end(self, trainer: lightning.Trainer, pl_module: lightning.LightningModule) -> None:
-        param_value = trainer.callback_metrics.get(self.param_name)
-        if param_value is not None:
-            param_value_item = param_value.item()
+        metric_value = trainer.callback_metrics.get(self.metric_name)
+        if metric_value is not None:
+            metric_value_item = metric_value.item()
             with open(self.output_path, "w") as f:
-                json.dump(param_value_item, f)
+                json.dump(metric_value_item, f)
         else:
-            print(f"Warning: '{self.param_name}' not found in callback_metrics.")
+            print(f"Warning: '{self.metric_name}' not found in callback_metrics.")
