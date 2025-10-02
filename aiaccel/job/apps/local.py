049e63610c2fd79a80d5ef312a4334b3c2c562c2
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-10-02 16:43:50 +0900

    Update jobs

--- aiaccel/job/apps/local.py
+++ aiaccel/job/apps/local.py
@@ -1,26 +1,31 @@
 #! /usr/bin/env python3
 
-from typing import Any
 
-from argparse import ArgumentParser, Namespace
 from functools import partial
-from importlib import resources
 import logging
 from multiprocessing import Pool
-import os
 from pathlib import Path
 import shlex
 import subprocess
 
-from aiaccel.config import load_config, overwrite_omegaconf_dumper, print_config, resolve_inherit
+from aiaccel.job.apps import prepare_argument_parser
 
 logger = logging.getLogger(__name__)
-overwrite_omegaconf_dumper()
 
 
-def dispatch_job(mode: str, args: Namespace, config: Any) -> None:
+def main() -> None:
+    # Load configuration (from the default YAML string)
+    config, parser, sub_parsers = prepare_argument_parser("local.yaml")
+
+    args = parser.parse_args()
+    mode = args.mode + "-array" if getattr(args, "n_tasks", None) is not None else args.mode
+
+    for key in ["walltime", "n_nodes"]:
+        if getattr(args, key, None) is not None:
+            logger.warning(f"Argument '{key}' is defined for compatibility and will not be used in aiaccel-job local.")
+
     # Prepare the job script and arguments
-    job = config[mode]["job"].format(command=shlex.join(args.command), args=args)
+    job = config[mode].job.format(command=shlex.join(args.command), args=args)
 
     if mode in ["cpu-array", "gpu-array"]:
         job = f"TASK_STEPSIZE={args.n_tasks_per_proc} {job}"
@@ -36,7 +41,7 @@ trap 'exit $?' ERR EXIT  # at error and exit
 trap 'echo 143' TERM  # at termination (by job scheduler)
 
 
-{config["script_prologue"]}
+{config.script_prologue}
 
 {job} 2>&1 | tee {log_filename}
 """
@@ -59,67 +64,6 @@ trap 'echo 143' TERM  # at termination (by job scheduler)
                 pass
     else:
         subprocess.run(f"bash {job_filename}", shell=True, check=True)
-
-
-def main() -> None:
-    # Load configuration (from the default YAML string)
-    parser = ArgumentParser(add_help=False)
-    parser.add_argument("--print_config", action="store_true")
-    parser.add_argument("--config", type=Path, default=None)
-    args, _ = parser.parse_known_args()
-
-    base_config_path = resources.files(f"{__package__}.config")
-    args.config = Path(args.config or os.environ.get("AIACCEL_JOB_CONFIG") or base_config_path / "local.yaml")  # type: ignore
-
-    config = load_config(
-        args.config,
-        {
-            "config_path": args.config,
-            "base_config_path": str(base_config_path),
-        },
-    )
-
-    if args.print_config:
-        print_config(config)
-
-    config = resolve_inherit(config)
-
-    # Parse command-line arguments
-    parser = ArgumentParser()
-    parser.add_argument("--print_config", action="store_true")
-    parser.add_argument("--config", type=Path)
-    sub_parsers = parser.add_subparsers(dest="mode", required=True)
-
-    parent_parser = ArgumentParser(add_help=False)
-    parent_parser.add_argument("--walltime", type=str)  # defined for compatibility
-    parent_parser.add_argument("log_filename", type=Path)
-    parent_parser.add_argument("command", nargs="+")
-
-    sub_parser = sub_parsers.add_parser("cpu", parents=[parent_parser])
-    sub_parser.add_argument("--n_tasks", type=int)
-    sub_parser.add_argument("--n_tasks_per_proc", type=int, default=config.cpu_array.n_tasks_per_proc)
-    sub_parser.add_argument("--n_procs", type=int, default=config.cpu_array.n_procs)
-
-    sub_parser = sub_parsers.add_parser("gpu", parents=[parent_parser])
-    sub_parser.add_argument("--n_tasks", type=int)
-    sub_parser.add_argument("--n_tasks_per_proc", type=int, default=config.gpu_array.n_tasks_per_proc)
-    sub_parser.add_argument("--n_procs", type=int, default=config.gpu_array.n_procs)
-
-    sub_parser = sub_parsers.add_parser("mpi", parents=[parent_parser])
-    sub_parser.add_argument("--n_procs", type=int, required=True)
-    sub_parser.add_argument("--n_nodes", type=int)  # defined for compatibility
-
-    sub_parser = sub_parsers.add_parser("train", parents=[parent_parser])
-    sub_parser.add_argument("--n_gpus", type=int)
-
-    args = parser.parse_args()
-    mode = args.mode + "-array" if getattr(args, "n_tasks", None) is not None else args.mode
-
-    for key in ["walltime", "n_nodes"]:
-        if getattr(args, key, None) is not None:
-            logger.warning(f"Argument '{key}' is defined for compatibility and will not be used in aiaccel-job local.")
-
-    dispatch_job(mode, args, config)
 
 
 if __name__ == "__main__":
--- aiaccel/job/apps/pbs.py
+++ aiaccel/job/apps/pbs.py
@@ -1,21 +1,23 @@
 #! /usr/bin/env python
 
-from typing import Any
 
-from argparse import ArgumentParser, Namespace
-from importlib import resources
-import os
 from pathlib import Path
 import shlex
 import subprocess
 import time
 
-from aiaccel.config import load_config, print_config, resolve_inherit
+from aiaccel.job.apps import prepare_argument_parser
 
 
-def dispatch_job(mode: str, args: Namespace, config: Any) -> None:
+def main() -> None:
+    # Load configuration (from the default YAML string)
+    config, parser, sub_parsers = prepare_argument_parser("pbs.yaml")
+
+    args = parser.parse_args()
+    mode = args.mode + "-array" if getattr(args, "n_tasks", None) is not None else args.mode
+
     # Prepare the job script and arguments
-    job = config[mode]["job"].format(command=shlex.join(args.command), args=args)
+    job = config[mode].job.format(command=shlex.join(args.command), args=args)
 
     if mode in ["cpu-array", "gpu-array"]:
         job = f"""\
@@ -65,13 +67,13 @@ if [ -n "$PBS_O_WORKDIR" ] && [ "$PBS_ENVIRONMENT" != "PBS_INTERACTIVE" ]; then
 fi
 
 
-{config["script_prologue"]}
+{config.script_prologue}
 
 {job}
 """
 
-    qsub = config["qsub"].format(args=args)
-    qsub_args = config[mode]["qsub_args"].format(args=args)
+    qsub = config.qsub.format(args=args)
+    qsub_args = config[mode].qsub_args.format(args=args)
 
     # Create the job script file, remove old status files, and run the job
     args.log_filename.parent.mkdir(exist_ok=True, parents=True)
@@ -93,63 +95,6 @@ fi
         if status != 0:
             raise RuntimeError(f"Job failed with {status} exit code.")
         status_filename.unlink()
-
-
-def main() -> None:
-    # Load configuration (from the default YAML string)
-    parser = ArgumentParser(add_help=False)
-    parser.add_argument("--print_config", action="store_true")
-    parser.add_argument("--config", type=Path, default=None)
-    args, _ = parser.parse_known_args()
-
-    base_config_path = resources.files(f"{__package__}.config")
-    args.config = Path(args.config or os.environ.get("AIACCEL_JOB_CONFIG") or base_config_path / "pbs.yaml")  # type: ignore
-
-    config = load_config(
-        args.config,
-        {
-            "config_path": args.config,
-            "base_config_path": str(base_config_path),
-        },
-    )
-
-    if args.print_config:
-        print_config(config)
-
-    config = resolve_inherit(config)
-
-    # Parse command-line arguments
-    parser = ArgumentParser()
-    parser.add_argument("--print_config", action="store_true")
-    parser.add_argument("--config", type=Path)
-    sub_parsers = parser.add_subparsers(dest="mode", required=True)
-
-    parent_parser = ArgumentParser(add_help=False)
-    parent_parser.add_argument("--walltime", type=str, default=config.walltime)
-    parent_parser.add_argument("log_filename", type=Path)
-    parent_parser.add_argument("command", nargs="+")
-
-    sub_parser = sub_parsers.add_parser("cpu", parents=[parent_parser])
-    sub_parser.add_argument("--n_tasks", type=int)
-    sub_parser.add_argument("--n_tasks_per_proc", type=int, default=config.cpu_array.n_tasks_per_proc)
-    sub_parser.add_argument("--n_procs", type=int, default=config.cpu_array.n_procs)
-
-    sub_parser = sub_parsers.add_parser("gpu", parents=[parent_parser])
-    sub_parser.add_argument("--n_tasks", type=int)
-    sub_parser.add_argument("--n_tasks_per_proc", type=int, default=config.gpu_array.n_procs)
-    sub_parser.add_argument("--n_procs", type=int, default=config.gpu_array.n_procs)
-
-    sub_parser = sub_parsers.add_parser("mpi", parents=[parent_parser])
-    sub_parser.add_argument("--n_procs", type=int, required=True)
-    sub_parser.add_argument("--n_nodes", type=int, default=config.mpi.n_nodes)
-
-    sub_parser = sub_parsers.add_parser("train", parents=[parent_parser])
-    sub_parser.add_argument("--n_gpus", type=int)
-
-    args = parser.parse_args()
-    mode = args.mode + "-array" if getattr(args, "n_tasks", None) is not None else args.mode
-
-    dispatch_job(mode, args, config)
 
 
 if __name__ == "__main__":
