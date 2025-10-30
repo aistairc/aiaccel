7853684fc23adae078ac35d98be31399b6d638f5
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-10-30 16:29:07 +0900

    Update argument parsing and config value selection
    Refactor argument parsing and value retrieval logic.

--- aiaccel/config/apps/get_value.py
+++ aiaccel/config/apps/get_value.py
@@ -1,5 +1,7 @@
 import argparse
 
+from omegaconf import OmegaConf as oc
+
 from aiaccel.config.config import load_config, overwrite_omegaconf_dumper, resolve_inherit
 
 
@@ -8,18 +10,13 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="Configuration file path")
-    parser.add_argument("--key", nargs="*", help="Target key in configration file")
+    parser.add_argument("key", help="Target key in the configration file")
 
     args, _ = parser.parse_known_args()
     config = load_config(args.config)
     config = resolve_inherit(config)
 
-    value = config
-    for k in args.key:
-        if value is not None:
-            value = value.get(k)
-
-    print(value)
+    print(oc.select(config, key)
 
 
 if __name__ == "__main__":
