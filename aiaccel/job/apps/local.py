bf00b68096a9397a05c44b8da8ad9f4b1b587421
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-10-30 21:22:48 +0900

    Feature/fix job local (#517)
    * bugfix
    
    * bugfix
    
    * bugfix

--- aiaccel/job/apps/local.py
+++ aiaccel/job/apps/local.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
 
 
-from functools import partial
 import logging
 from math import ceil
-from multiprocessing import Pool
 from pathlib import Path
 import shlex
 import subprocess
@@ -21,7 +19,7 @@ def main() -> None:
     args = parser.parse_args()
     mode = args.mode + "-array" if getattr(args, "n_tasks", None) is not None else args.mode
 
-    for key in ["walltime", "n_nodes", "n_tasks_per_procs"]:
+    for key in ["walltime", "n_nodes", "n_tasks_per_proc"]:
         if getattr(args, key, None) is not None:
             logger.warning(f"Argument '{key}' is defined for compatibility and will not be used in aiaccel-job local.")
 
@@ -40,7 +38,7 @@ for LOCAL_PROC_INDEX in {{1..{args.n_procs}}}; do
 
     TASK_INDEX=$TASK_INDEX \\
     TASK_STEPSIZE={n_tasks_per_proc} \\
-        {job} > {args.log_filename.with_suffix("")}.${{LOCAL_PROC_INDEX}}.log 2>&1 &
+        {job} 2>&1 | tee {args.log_filename.with_suffix("")}.${{LOCAL_PROC_INDEX}}.log &
 
     pids[$LOCAL_PROC_INDEX]=$!
 done
@@ -49,9 +47,8 @@ for i in "${{!pids[@]}}"; do
     wait ${{pids[$i]}}
 done
 """
-        log_filename = f"{args.log_filename.with_suffix('')}.${{TASK_INDEX}}.log"
     else:
-        log_filename = args.log_filename
+        job = f"{job} 2>&1 | tee {args.log_filename}"
 
     job_script = f"""\
 #! /bin/bash
@@ -59,11 +56,12 @@ done
 set -eE -o pipefail
 trap 'exit $?' ERR EXIT  # at error and exit
 trap 'echo 143' TERM  # at termination (by job scheduler)
+trap 'kill 0' INT EXIT
 
 
 {config.script_prologue}
 
-{job} 2>&1 | tee {log_filename}
+{job}
 """
 
     # Create the job script file, remove old status files, and run the job
@@ -73,17 +71,7 @@ trap 'echo 143' TERM  # at termination (by job scheduler)
     with open(job_filename, "w") as f:
         f.write(job_script)
 
-    if mode in ["cpu-array", "gpu-array"]:
-        worker = partial(subprocess.run, shell=True, check=True)
-        with Pool(processes=args.n_procs) as pool:
-            cmd_list = []
-            for ii in range(0, args.n_tasks, args.n_tasks_per_proc):
-                cmd_list.append(f"TASK_INDEX={ii + 1} bash {job_filename}")
-
-            for _ in pool.imap_unordered(worker, cmd_list):
-                pass
-    else:
-        subprocess.run(f"bash {job_filename}", shell=True, check=True)
+    subprocess.run(f"bash {job_filename}", shell=True, check=True)
 
 
 if __name__ == "__main__":
