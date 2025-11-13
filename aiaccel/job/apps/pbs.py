f306f2f9eecf31c4697e8adf9f28b17a21890041
Yoshiaki Bando <yoshipon@users.noreply.github.com>
2025-11-13 14:03:36 +0900

    Update comments
    Refreshed the file system conditionally before reading the status.

--- aiaccel/job/apps/pbs.py
+++ aiaccel/job/apps/pbs.py
@@ -91,8 +91,8 @@ fi
     for status_filename in status_filename_list:
         while not status_filename.exists():
             time.sleep(1.0)
-            # Update the file system
-            if getattr(args, "use_scandir", False):
+
+            if getattr(args, "use_scandir", False):  # Reflesh the file system if needed
                 os.scandir()
 
         status = int(status_filename.read_text())
