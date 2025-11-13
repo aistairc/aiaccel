8d009c4a9b9b05755216d721b97945d171ca00bd
KanaiYuma <kanai-itc1013@aist.go.jp>
2025-11-13 16:23:08 +0900

    Add scandir.

--- aiaccel/job/apps/slurm.py
+++ aiaccel/job/apps/slurm.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 
+import os
 from pathlib import Path
 import shlex
 import subprocess
@@ -83,6 +84,7 @@ trap 'echo $? > {job_status_filename}' ERR EXIT  # at error and exit
     for status_filename in status_filename_list:
         while not status_filename.exists():
             time.sleep(1.0)
+            os.scandir(status_filename.parent)
 
         status = int(status_filename.read_text())
         if status != 0:
