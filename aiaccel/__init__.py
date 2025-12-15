4657f95b39a6972305cfdc68e5b09f19b9fce5bf
Takashi OKADA <tk.okada.github@gmail.com>
2025-12-15 09:12:00 +0900

    Rollback small changes.

--- aiaccel/__init__.py
+++ aiaccel/__init__.py
@@ -1,9 +1,6 @@
 # Copyright (C) 2025 National Institute of Advanced Industrial Science and Technology (AIST)
 # SPDX-License-Identifier: MIT
 
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
-try:
-    __version__ = version(__package__ or "aiaccel")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = version(__package__)
