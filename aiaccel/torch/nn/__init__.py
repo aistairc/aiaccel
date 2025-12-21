16de721c394c046334bce7ebb202b1491ac3f278
ssakurai422 <sakurai.shun@aist.go.jp>
2025-12-21 17:55:48 +0900

    fix init

--- aiaccel/torch/nn/__init__.py
+++ aiaccel/torch/nn/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2025 National Institute of Advanced Industrial Science and Technology (AIST)
 # SPDX-License-Identifier: MIT
 
-from m3l.common.nn.sinusoidal_positional_embedding import SinusoidalPositionalEmbedding
+from aiaccel.torch.nn.sinusoidal_positional_embedding import SinusoidalPositionalEmbedding
 
 __all__ = [
     "SinusoidalPositionalEmbedding",
