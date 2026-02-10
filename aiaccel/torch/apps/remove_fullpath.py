5b8e9ea4b5f1f8cf4a8fedba5cf9a8bf9782e1ee
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-02-10 15:31:27 +0900

    Add Upload Hugging Face.

--- aiaccel/torch/apps/remove_fullpath.py
+++ aiaccel/torch/apps/remove_fullpath.py
@@ -10,6 +10,8 @@ from omegaconf import OmegaConf as oc  # noqa: N813
 
 import torch
 
+from huggingface_hub import login, upload_file
+
 
 def remove_fullpath(obj: dict[str, Any] | list[Any] | Any) -> Any:
     """Recursively remove elements containing full paths from a dict.
@@ -38,6 +40,9 @@ def main() -> None:
     parser.add_argument("--save_config_filename", type=str, default="pathremoved_config.yaml")
     parser.add_argument("--ckpt_path", type=Path)
     parser.add_argument("--save_ckpt_filename", type=str, default="pathremoved.ckpt")
+    parser.add_argument("--repo_id", type=str)
+    parser.add_argument("--repo_type", type=str)
+
     args = parser.parse_args()
 
     config = oc.to_container(oc.load(args.config_path))
@@ -49,6 +54,23 @@ def main() -> None:
     ckpt = remove_fullpath(ckpt)
 
     torch.save(ckpt, args.ckpt_path.parent / args.save_ckpt_filename)
+
+    if args.repo_id and args.repo_type:
+        # Upload config and ckpt in Hugging Face
+        login()
+
+        upload_file(
+            path_or_fileobj=args.config_path.parent / args.save_config_filename,
+            path_in_repo=args.save_config_filename,
+            repo_id=args.repo_id,
+            repo_type=args.repo_type,
+        )
+        upload_file(
+            path_or_fileobj=args.ckpt_path.parent / args.save_ckpt_filename,
+            path_in_repo=args.save_ckpt_filename,
+            repo_id=args.repo_id,
+            repo_type=args.repo_type,
+        )
 
 
 if __name__ == "__main__":
