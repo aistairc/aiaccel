ffbf9fa4159e6476499a5937044f9d2274658b1e
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-02-13 14:53:49 +0900

    Add dependency stage n-1.

--- aiaccel/workflow/apps/templete_files/stages.mk
+++ aiaccel/workflow/apps/templete_files/stages.mk
@@ -5,7 +5,7 @@
 STAGES = $(foreach num_of_stage,$(shell seq $(min_stage) $(max_stage)),stage$(num_of_stage))
 
 .NOTPARALLEL: check_train_artifacts 
-.PHONY: all clean status scores check_train_artifacts $(STAGES) $(STAGES:%=skip-%) 
+.PHONY: all clean status scores check_train_artifacts $(STAGES) $(STAGES:%=skip-%)
 .WAIT:
 
 # general rules
@@ -18,7 +18,11 @@ status:
 	@$(foreach var,$(PRINT_VARIABLES),echo $(var): $($(var));)
 	@echo ================================================================
 
-$(STAGES): status .WAIT | $($@_dependencies)
+$(foreach n,$(shell seq $$(($(min_stage)+1)) $(max_stage)), \
+  $(eval stage$(n): status .WAIT $(stage$(n)_dependencies) stage$(shell echo $$(( $(n) - 1 )))) \
+)
+
+$(STAGES):
 	@echo "\033[1;34m$@ finished\033[0m"
 
 $(STAGES:%=skip-%):
