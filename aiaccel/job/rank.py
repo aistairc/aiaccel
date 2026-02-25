a6e55be7e6eb010d59aab5362b58c78946e04f4a
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-02-25 13:43:26 +0900

    Move get_rank in job.

---
+++ aiaccel/job/rank.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2025 National Institute of Advanced Industrial Science and Technology (AIST)
+# SPDX-License-Identifier: MIT
+
+import os
+
+
+def get_rank(default: int = 0) -> int:
+    for key in [
+        "GLOBAL_RANK",  # PyTorch Lightning
+        "RANK",  # torchrun / deepspeed / pytorch launcher
+        "OMPI_COMM_WORLD_RANK",  # OpenMPI
+        "PMI_RANK",  # MPICH / Intel MPI
+        "MV2_COMM_WORLD_RANK",  # MVAPICH2
+        "SLURM_PROCID",  # Slurm
+    ]:
+        rank = os.environ.get(key)
+        if rank is not None:
+            try:
+                return int(rank)
+            except ValueError:
+                pass
+
+    return default
