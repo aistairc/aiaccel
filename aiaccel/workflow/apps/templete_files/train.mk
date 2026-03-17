6f65dbe4fe77b0936d8d5de599b129910df9854f
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-03-17 17:05:17 +0900

    Fix train.mk.

--- aiaccel/workflow/apps/templete_files/train.mk
+++ aiaccel/workflow/apps/templete_files/train.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2025 National Institute of Advanced Industrial Science and Technology (AIST)
 # SPDX-License-Identifier: MIT
 
-$(train_path)/.train.done: | stage$(train_stage) $(train_path)/config.yaml
+$(train_path)/.train.done: | stage$(pre_train_stage) $(train_path)/config.yaml
 	@set -e; \
 	set -- \
 		$(train_path)/train.* \
@@ -15,10 +15,12 @@ $(train_path)/.train.done: | stage$(train_stage) $(train_path)/config.yaml
 		echo "Found existing files/directories:"; printf '  %s\n' $$existing; \
 		read -r -p "Delete these? [y/N]: " ans; \
 		case "$$ans" in y|Y) rm -r $$existing ;; *) echo "Aborted."; exit 1 ;; esac; \
-	fi
+	fi; \
+	n_gpus=$$(aiaccel-config get-value $(train_path)/config.yaml n_gpus); \
+	walltime=$$(aiaccel-config get-value $(train_path)/config.yaml walltime); \
 	$(cmd) train \
-		--n_gpus=$(shell aiaccel-config get-value $(train_path)/config.yaml n_gpus) \
-		--walltime=$(shell aiaccel-config get-value $(train_path)/config.yaml walltime) \
+		--n_gpus=$$n_gpus \
+		--walltime=$$walltime \
 		$(job_ops) $(train_path)/train.log -- \
-			python -m aiaccel.torch.apps.train $(train_path)/config.yaml
+			python -m aiaccel.torch.apps.train $(abspath $(train_path)/config.yaml)
 	touch $@
