2a62318cebba094cbe0b28450282128fc021dc43
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-03-31 13:58:28 +0900

    Fix print.

--- aiaccel/job/apps/slurm.py
+++ aiaccel/job/apps/slurm.py
@@ -79,7 +79,11 @@ trap 'echo $? > {job_status_filename}' ERR EXIT  # at error and exit
     if job_filename.exists() and all(
         _is_success_status_file(status_filename) for status_filename in status_filename_list
     ):
-        print(f"A successfully completed .out file exists({status_filename_list}), so the job will not be submitted.")
+        print(
+            "A successfully completed .out file exists"
+            f"({[str(status_filename) for status_filename in status_filename_list]}), "
+            "so the job will not be submitted."
+        )
         for status_filename in status_filename_list:
             status_filename.unlink(missing_ok=True)
         return
