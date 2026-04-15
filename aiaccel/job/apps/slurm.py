0eb745ff3ee8aeaefa69aab3c44c2c8ff56682c3
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-04-15 15:00:02 +0900

    Fix typo in aiaccel.

--- aiaccel/job/apps/slurm.py
+++ aiaccel/job/apps/slurm.py
@@ -87,7 +87,7 @@ trap 'echo $? > {job_status_filename}' ERR EXIT  # at error and exit
         while not status_filename.exists():
             time.sleep(1.0)
 
-            if config.get("use_scandir", False):  # Reflesh the file system if needed
+            if config.get("use_scandir", False):  # Refresh the file system if needed
                 os.scandir(status_filename.parent)
 
         status = int(status_filename.read_text())
