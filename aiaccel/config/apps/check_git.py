2c5ee3313c2abefca018854193c615b1b1f7601a
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-05-27 09:42:13 +0900

    Check not_ready_status.

--- aiaccel/config/apps/check_git.py
+++ aiaccel/config/apps/check_git.py
@@ -16,7 +16,10 @@ def main() -> None:
     args, unk_args = parser.parse_known_args()
     config = prepare_config(args.config, overwrite_config=oc.from_cli(unk_args))
 
-    if len(git_status := collect_git_status_from_config(config)) > 0:
+    git_status = collect_git_status_from_config(config)
+    not_ready_status = [status for status in git_status if not status.ready()]
+
+    if len(not_ready_status) > 0:
         print_git_status(git_status)
 
         exit(1)
