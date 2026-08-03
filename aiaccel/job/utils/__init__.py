407da4cb08fee5243fb30208ac58de51e90a1ee2
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-08-03 10:46:15 +0900

    Rename get_task_info and move split_tasks.

--- aiaccel/job/utils/__init__.py
+++ aiaccel/job/utils/__init__.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MIT
 
 from aiaccel.job.utils.get_rank import get_rank
-from aiaccel.job.utils.get_task_environment import get_task_index, get_task_stepsize
-from aiaccel.job.utils.split_tasks import split_tasks
+from aiaccel.job.utils.get_task_info import get_task_index, get_task_stepsize, split_tasks
 
 __all__ = ["get_rank", "split_tasks", "get_task_index", "get_task_stepsize"]
