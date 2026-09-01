207f24ba0be2bda909050f184196b41ddc98a1bd
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-09-01 10:49:55 +0900

    Add pbs test.

---
+++ tests/job/apps/pbs.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2025 National Institute of Advanced Industrial Science and Technology (AIST)
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+import subprocess
+
+import pytest
+
+cmd = ["aiaccel-job", "pbs"]
+
+
+def test_cpu(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.chdir(tmp_path)
+
+    log_path = tmp_path / "test.log"
+
+    config_path = Path(__file__).parent / "config" / "custom_pbs.yaml"
+
+    subprocess.run(
+        cmd + ["--config", config_path, "cpu", log_path, "--", "echo", "hello"],
+        check=True,
+    )
+
+    assert log_path.exists()
+    assert log_path.read_text() == "hello\n"
