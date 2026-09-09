0425a660ecef76038c70408ea7162d3c434e8e62
KanaiYuma <kanai-itc1013@aist.go.jp>
2026-09-09 13:30:58 +0900

    Debug.

--- tests/job/apps/test_slurm.py
+++ tests/job/apps/test_slurm.py
@@ -3,6 +3,9 @@
 
 from pathlib import Path
 import subprocess
+import time
+
+import pytest
 
 from tests.job.apps.scheduler_test_base import SchedulerTestBase
 
@@ -32,3 +35,57 @@ class TestSlurm(SchedulerTestBase):
             ["scancel", job_id],
             check=True,
         )
+
+    def test_cpu_failure(
+        self,
+        tmp_path: Path,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> None:
+        monkeypatch.chdir(tmp_path)
+
+        log_path = tmp_path / "test.log"
+        status_path = tmp_path / "test.out"
+
+        process = subprocess.Popen(
+            self.make_command()
+            + [
+                "cpu",
+                log_path,
+                "--",
+                "bash",
+                "-c",
+                "exit 7",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        assert process.stdout is not None
+
+        job_id = self.get_job_id(process.stdout.readline())
+
+        for _ in range(40):
+            result = subprocess.run(
+                ["squeue", "-j", job_id, "-h", "-o", "%T %R"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+
+            print(f"{job_id=}, {result.stdout=!r}")
+
+            if result.stdout.startswith("RUNNING"):
+                break
+
+            time.sleep(1)
+
+        _, stderr = process.communicate(timeout=30)
+
+        assert process.returncode == 1
+        assert "Job failed with 7 exit code." in stderr
+
+        self.wait_for_job_to_finish(job_id)
+
+        assert status_path.exists()
+        assert status_path.read_text().strip() == "7"
